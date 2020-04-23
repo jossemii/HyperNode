c26c806e1f95a6e4a73c3b13f29b89b258211d8a
jal04 <josemi.bnf@outlook.es>
2020-04-23 16:53:59 +0000

    structs added

--- start.go
+++ start.go
@@ -5,8 +5,27 @@ import (
 	"os"
 )
 
+// Tensor struct
+type Tensor struct {
+	ID string `json:"id"`
+
+	DEF string `json:"definition"`
+
+	CONTAINERS []Container `json:"containers"`
+}
+
+// Container struct
+type Container struct {
+	ID string `json:"id"`
+
+	SCORE int `json:"score"`
+
+	GIT string `json:"git"`
+}
+
 func getthebest(tensor string) {
 	fmt.Printf("Founding way to resolve ...  %#v\n", tensor)
+
 	// Open our jsonFile
 	jsonFile, err := os.Open("data.json")
 	// if we os.Open returns an error then handle it
@@ -14,6 +33,7 @@ func getthebest(tensor string) {
 		fmt.Println(err)
 	}
 	fmt.Println("Successfully Opened data.json")
+
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 }
