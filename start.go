83f6fecd5d3486dcfd311362ec56dc9c0cbd9a7d
jal04 <josemi.bnf@outlook.es>
2020-04-23 16:42:52 +0000

    getthebest added

--- start.go
+++ start.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+func getthebest(tensor string) {
+	fmt.Printf("Founding way to resolve ...  %#v\n", tensor)
+	// Open our jsonFile
+	jsonFile, err := os.Open("data.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened data.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+}
+
 func main() {
-	print("Starting node\n")
-	
+	fmt.Print("Starting node\n")
+	var tensor string
+	fmt.Print("Select the problem.. tensor:  ")
+	fmt.Scanf("%s", &tensor)
+	getthebest(tensor)
 }
