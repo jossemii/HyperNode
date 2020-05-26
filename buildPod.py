71928577f7234980afe63d2cb8cb3b86054c488e
jal04 <josemi.bnf@outlook.es>
2020-05-26 23:30:10 +0200

    get pkg lines in build OK

--- buildPod.py
+++ buildPod.py
@@ -12,7 +12,6 @@ class Pod:
         self.isAbstract = True
 
     def setPkg(self,key,line):
-        print(key, line)
         self.Pkgs.update({key:line})
 
     def setApi(self, line):
@@ -32,30 +31,32 @@ class Pod:
         print('Packages ',self.Pkgs)
         print('Abstract ',self.isAbstract)
     
+    @staticmethod
+    def makePod(filename):
+        def switch(line, pod):
+            s = line[0]
+            if s.startswith('PKG'):
+                pod.setPkg(s[3],line[1])
+            elif s == 'API':
+                pod.setApi(line[1:])
+            elif s == 'CTR':
+                pod.setCtr(line[1:])
+            elif s == 'TNS':
+                pod.setTensor(line[1:])
+        file = open(filename, "r")
+        pod = Pod()
+        for l in file.readlines():
+            try:
+                switch( l.split(), pod )
+            except IndexError:
+                break
+        return pod
+
     def build(self):
-        pass
+        for k in self.Pkgs.keys():
+            line = self.Pkgs.get(k)
+            print(line)
 
-
-
-def makePod(filename):
-    def switch(line, pod):
-        s = line[0]
-        if s.startswith('PKG'):
-            pod.setPkg(s[3],line[1])
-        elif s == 'API':
-            pod.setApi(line[1:])
-        elif s == 'CTR':
-            pod.setCtr(line[1:])
-        elif s == 'TNS':
-            pod.setTensor(line[1:])
-    file = open(filename, "r")
-    pod = Pod()
-    for l in file.readlines():
-        try:
-            switch( l.split(), pod )
-        except IndexError:
-            break
-    return pod
 
 def isValidHyperFile(file):
     return True
@@ -63,6 +64,6 @@ def isValidHyperFile(file):
 if __name__ == "__main__":
     file="hyperfiles/frontier.hy"
     if isValidHyperFile(file):
-        pod = makePod(file)
-        pod.show()
+        pod = Pod.makePod(file)
+        #pod.show()
         pod.build()
