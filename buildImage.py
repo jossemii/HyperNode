a192bc3f84162c414d8a6feecdeae1aeec32fb73
jal04 <josemi.bnf@outlook.es>
2020-06-01 13:45:29 +0200

    makePod deleted

--- buildImage.py
+++ buildImage.py
@@ -10,29 +10,6 @@ class Image:
         self.image = image
 
     @staticmethod
-    def makePod(filename):
-        def get_this_file():
-            pass
-        def switch(line, pod):
-            s = line[0]
-            if s.startswith('.PKG'):
-                pod.setPkg(get_this_file())
-            elif s == 'API':
-                pod.setApi(line[1:])
-            elif s == 'CTR':
-                pod.setCtr(line[1:])
-            elif s == 'TNS':
-                pod.setTensor(line[1:])
-        file = open(filename, "r")
-        image = Image()
-        for l in file.readlines():
-            try:
-                switch( l.split(), image )
-            except IndexError:
-                break
-        return image
-
-    @staticmethod
     def makeImage(filename):
         file = json.load(open(filename,"r"))
         return Image(file)
@@ -52,6 +29,6 @@ def isValidHyperFile(file):
 if __name__ == "__main__":
     file="frontier/Hyperfile.json"
     if isValidHyperFile(file):
-        image = Image.makePod(file)
+        image = Image.makeImage(file)
         image.show()
         #image.build()
