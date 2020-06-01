c959146a2b81c72b791fe9841bb80a3044645d6c
jal04 <josemi.bnf@outlook.es>
2020-06-01 17:11:56 +0200

    socket

--- tester_exemple.py
+++ tester_exemple.py
@@ -5,5 +5,5 @@ def random_cnf():
     pass
 
 if __name__ == "__main__":
-    response = requests.get("hyper://3723c39d43fc", params=random_cnf())
+    response = requests.get("http://localhost/", params=random_cnf())
     best_interpretation = response.text()
