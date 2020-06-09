21be8bab282431b4134d87eb49bc3ab3aab97768
jal04 <josemi.bnf@outlook.es>
2020-06-09 22:44:08 +0200

    rebuild and container id are different

--- gateway.py
+++ gateway.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     def get(image):
         container_id = buildImage.ok(str(image)) # Si no esta construido, lo construye.
         pod_port = buildImage.select_port()
-        #run('docker exec ',container_id,' --port ',pod_port) # Ejecuta una instancia de la imagen con el puerto que sea.
+        run('docker run -p '+pod_port+':'+pod_port+' '+container_id) # Ejecuta una instancia de la imagen con el puerto que sea.
         pod_api = '/'
         return 'http://0.0.0.0:'+pod_port+pod_api
 
