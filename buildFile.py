13b47614046fb7571654fad68ee1bbb8eae96244
jal04 <josemi.bnf@outlook.es>
2020-06-09 13:41:01 +0200

    buildFile se encarga de crear un Hyperfile.json, tomar un Dockerfile para el build, calcular la hash ......

---
+++ buildFile.py
@@ -0,0 +1,26 @@
+import sys
+import json
+
+def DockerfileToString(Dockername):
+        #Read Dockerfile
+    Dockerfile = open(Dockername, "r")
+    build = ""
+    for l in Dockerfile.readlines():
+        build = build+l
+    print(build,"\n")
+    Dockerfile.close()
+    return build
+
+def writeBuild(Hypername, string):
+    Hyperfile = json.load(open(Hypername,"r"))
+    Hyperfile.update({'BUILD': string})
+    print(Hyperfile)
+    return( json.dumps(Hyperfile, indent=4, sort_keys=True) )
+
+if __name__ == "__main__":
+    Hyperfile = sys.argv[1]  # Hyperfile
+    Dockerfile = sys.argv[2] # Dockerfile
+    string = DockerfileToString(Dockerfile)
+    json = writeBuild(Hyperfile, string)
+    with open(Hyperfile, "w") as json_file:
+        json_file.write(json)
