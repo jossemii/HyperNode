a08750b83ac172e250a8988ac753d91f1c74c817
jal04 <josemi.bnf@outlook.es>
2020-06-09 13:07:15 +0200

    response 200 OK

--- execImage.py
+++ execImage.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
     app = Flask(__name__)
 
     @app.route('/<image>')
-    def get(self, name):
-        if name=='3723c39d43fc':
+    def get(image):
+        if image=='3723c39d43fc':
             return 'http://0.0.0.0:8000'
         else:
             return 404
 
     @app.route('/delete/<port_uri>')
-    def delete(self, name):
+    def delete(port_uri):
         return 404
  
     app.run(host='0.0.0.0', port=8080)
