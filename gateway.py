f119ba4d77f4a0c023a675236c15526d7ee69771
josemibnf <josemi.bnf@outlook.es>
2020-06-16 13:40:29 +0200

    las direcciones uri siempre son http://...:port/

--- gateway.py
+++ gateway.py
@@ -6,12 +6,16 @@ if __name__ == "__main__":
 
     app = Flask(__name__)
 
+    @app.route('/')
+    def hello():
+        return 'HELLO HYPER'
+
     @app.route('/<image>')
     def get(image):
         pod_id, api_port = buildImage.ok(str(image)) # Si no esta construido, lo construye.
         pod_port = buildImage.select_port()
         run('docker run -p '+pod_port+':'+api_port+" "+pod_id+'.oci') # Ejecuta una instancia de la imagen con el puerto que sea.
-        return 'http://0.0.0.0:'+pod_port
+        return 'http://0.0.0.0:'+pod_port+'/'
 
     @app.route('/delete/<port_uri>')
     def delete(port_uri):
--- orchest_pod_exemple.py
+++ orchest_pod_exemple.py
@@ -1,4 +1,4 @@
-from pip._vendor import requests
+import requests
 
 gateway_uri = 'http://0.0.0.0:8080/'
 gateway_uri_delete = 'http://0.0.0.0:8080/delete/'
@@ -13,5 +13,6 @@ def get_image_uri(image):
     return response.text
 
 if __name__ == "__main__":
-    sovler_uri = get_image_uri('073b5dcc2248512fcfe0241db51a987b9f4cc7e9ea3093730435cf818acb6b4d')
-    response = requests.get( sovler_uri+'/', params={'cnf':random_cnf()})
+    #sovler_uri = get_image_uri('073b5dcc2248512fcfe0241db51a987b9f4cc7e9ea3093730435cf818acb6b4d')
+    #response = requests.get( sovler_uri+'/', params={'cnf':random_cnf()})
+    print(requests.get(gateway_uri))
