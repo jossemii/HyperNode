67c015e8f542f9f66815bd36a75016eef95583e5
josemibnf <josemi.bnf@outlook.es>
2020-07-01 19:27:31 +0200

    gateway get

--- gateway.py
+++ gateway.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     @app.route('/<dependency>')
     def get(dependency):
         api_port = buildImage.ok(str(dependency)) # Si no esta construido, lo construye.
-        os.system('docker run --detach '+dependency+'.oci') # Ejecuta una instancia de la imagen con el puerto pod_port.
+        container_id = subprocess.check_output('docker run --detach '+dependency+'.oci') # Ejecuta una instancia de la imagen.
+        container_ip = subprocess.check_output("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "+container_id)
         return {
             'uri': container_ip + api_port,
             'token': container_id
