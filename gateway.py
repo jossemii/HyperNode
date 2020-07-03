e7607dde79e5df217b6fd18b062cb99903b3511b
josemibnf <josemi.bnf@outlook.es>
2020-07-03 19:08:21 +0200

    igual no tiene api ..

--- gateway.py
+++ gateway.py
@@ -13,14 +13,17 @@ if __name__ == "__main__":
 
     @app.route('/<dependency>')
     def get(dependency):
-        api_port = buildImage.ok(str(dependency)) # Si no esta construido, lo construye.
-        container_id = subprocess.check_output('docker run --detach '+dependency+'.oci') # Ejecuta una instancia de la imagen.
-        container_ip = subprocess.check_output("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "+container_id)
-        return {
-            'uri': container_ip + api_port,
-            'token': container_id
-        }
-
+        if tieneAPI:
+            api_port = buildImage.ok(str(dependency)) # Si no esta construido, lo construye.
+            container_id = subprocess.check_output('docker run --detach '+dependency+'.oci') # Ejecuta una instancia de la imagen.
+            container_ip = subprocess.check_output("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "+container_id)
+            return {
+                'uri': container_ip + api_port,
+                'token': container_id
+            }
+        else:
+            pass
+        
     @app.route('/<token>')
     def delete(token):
         subprocess.check_output('docker rm '+token+' --force')
