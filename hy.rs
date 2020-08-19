67a81856bcc6eff797406a26988da39f173e75f2
josemibnf <josemi.bnf@outlook.es>
2020-08-19 20:44:35 +0200

    ...

--- hy.rs
+++ hy.rs
@@ -8,3 +8,17 @@ print('Init '+image+' image\n-------------\n')
 
 response = requests.get('http://'+'localhost'+'/'+image)
 print(response.json)
+
+#!/bin/bash
+
+# Timestamp Function
+timestamp() {
+    date +"%T"
+}
+
+echo "⏲️    $(timestamp): started build script..."
+GIT=$(which git)
+echo "Buscando GIT  ---->  $GIT"
+DOCKER=$(which docker)
+echo "Buscando DOCKER  ---->  $DOCKER"
+echo "Software necesario listo."
