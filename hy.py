4391962a73395c71db69b03c6336fa809454acee
josemibnf <josemi.bnf@outlook.es>
2020-09-13 17:35:11 +0200

    hy.py ...

--- hy.py
+++ hy.py
@@ -1,24 +1,52 @@
 import requests
 import sys
+import argparse
 
-image = sys.argv[1]
 gateway_host = '127.0.0.1:8080'
 
-print('Init '+image+' image\n-------------\n')
 
-response = requests.get('http://'+'localhost'+'/'+image)
-print(response.json)
+def docker_container_id_from_name(docker_name):
+    return 'token'
 
-#!/bin/bash
+def check_gateway():
+    return requests.get('http://'+gateway_host+'/') == 200
 
-# Timestamp Function
-timestamp() {
-    date +"%T"
-}
 
-echo "⏲️    $(timestamp): started build script..."
-GIT=$(which git)
-echo "Buscando GIT  ---->  $GIT"
-DOCKER=$(which docker)
-echo "Buscando DOCKER  ---->  $DOCKER"
-echo "Software necesario listo."
+
+def launch_instance(image):
+    with open('__registry__/'+image+'.json') as file:
+        envs = {}
+        for env in file.Container.Envs:
+            print('Valor para: '+env)
+            envs.update({
+                env:input()
+            })
+    response = requests.post('http://'+gateway_host+'/'+image, json=envs)
+    print(response)
+
+def clean_cache():
+    import shutil
+    shutil.rmtree('/__hycache__')
+
+def delete_instance(docker_name):
+    token = docker_container_id_from_name(docker_name)
+    response = requests.get('http://'+gateway_host+'/'+token)
+    if response == 200: print('DO IT.')
+
+def clean_image(image):
+    print('borra la imagen de Docker.')
+
+def images_list():
+    pass
+
+def instances_list():
+    pass
+
+def instance_output(docker_name):
+    import os
+    os.system(
+        'sudo docker logs --follow '+docker_name
+    )
+
+if __name__ == "__main__":
+    pass
