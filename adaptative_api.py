0bbe3593f6e641e32f936e3ba2f32a12f99c71ae
josemibnf <josemi.bnf@outlook.es>
2020-10-27 21:54:53 +0100

    adaptative_api script

--- adaptative_api.py
+++ adaptative_api.py
@@ -1,32 +1,50 @@
-#La mejor forma de resolver el problema podria ser con redes neuronales,
+import sys
+
+# La mejor forma de resolver el problema podria ser con redes neuronales,
 #  pero un algoritmo podria salvarnos de los casos mas simples.
 
+# Codigo de mierda.
 def quizas_quisiste_decir(request, service_api):
     if type(service_api) is list:
         for elem in service_api:
-            if type(elem) is str:
+            if type(elem) is dict or type(elem) is list :
                 maybe_correct_response = quizas_quisiste_decir(request, elem)
                 if maybe_correct_response: return maybe_correct_response
-            if request in elem:
+            elif type(elem) is str and request in elem:
+                # if requests in elem or request_in_elem()
                 return elem
     elif type(service_api) is dict:
-        for elem
+        for elem in service_api:
+            if type(elem) is dict or type(elem) is list:
+                maybe_correct_response = quizas_quisiste_decir(request, elem)
+                if maybe_correct_response: return maybe_correct_response
+            elif type(elem) is str and request in elem:
+                # if requests in elem or request_in_elem()
+                return elem
+        for elem in service_api.values():
+            if type(elem) is dict or type(elem) is list:
+                maybe_correct_response = quizas_quisiste_decir(request, elem)
+                if maybe_correct_response: return maybe_correct_response
+            elif type(elem) is str and request in elem:
+                # if requests in elem or request_in_elem()
+                return elem
+            
 
-def __main__():
-    request = 'start'
+
+if __name__== "__main__":
 
     service_api_list = [
 
         {
-            1:'select'
-            2:'train/start'
-            3:'train/stop'
+            '1':'select',
+            '2':'train/start',
+            '3':'train/stop',
         },
 
         [{
-            1:'select'
-            2:'train/start'
-            3:'train/stop'
+            '1':'select',
+            '2':'train/start',
+            '3':'train/stop',
         }],
 
         [
@@ -45,6 +63,7 @@ def __main__():
 
     ]
 
+    print(service_api_list)
     for service_api in service_api_list:
-        correct_requests = quizas_quisiste_decir(request, service_api)
+        correct_requests = quizas_quisiste_decir(sys.argv[1], service_api)
         print(correct_requests)
