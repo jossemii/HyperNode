8bcb6d86c9615477153e20667485d964b1e523c1
josemibnf <josemi.bnf@outlook.es>
2020-10-27 23:08:02 +0100

    created string.like method

--- adaptative_api.py
+++ adaptative_api.py
@@ -1,35 +1,35 @@
 import sys
 
+class string(str):
+    def like(self, other: str) -> bool:
+        return self.lower() in other.lower()
+
 # La mejor forma de resolver el problema podria ser con redes neuronales,
 #  pero un algoritmo podria salvarnos de los casos mas simples.
 
 # Codigo de mierda.
-def quizas_quisiste_decir(request, service_api):
+def quizas_quisiste_decir(request: str, service_api: dict or list) -> str:
     if type(service_api) is list:
         for elem in service_api:
             if type(elem) is dict or type(elem) is list :
                 maybe_correct_response = quizas_quisiste_decir(request, elem)
                 if maybe_correct_response: return maybe_correct_response
-            elif type(elem) is str and request in elem:
-                # if requests in elem or request_in_elem()
+            elif type(elem) is str and string(request).like(elem) :
                 return elem
     elif type(service_api) is dict:
         for elem in service_api:
             if type(elem) is dict or type(elem) is list:
                 maybe_correct_response = quizas_quisiste_decir(request, elem)
                 if maybe_correct_response: return maybe_correct_response
-            elif type(elem) is str and request in elem:
-                # if requests in elem or request_in_elem()
+            elif type(elem) is str and string(request).like(elem) :
                 return elem
         for elem in service_api.values():
             if type(elem) is dict or type(elem) is list:
                 maybe_correct_response = quizas_quisiste_decir(request, elem)
                 if maybe_correct_response: return maybe_correct_response
-            elif type(elem) is str and request in elem:
-                # if requests in elem or request_in_elem()
+            elif type(elem) is str and string(request).like(elem) :
                 return elem
             
-
 
 if __name__== "__main__":
 
@@ -63,7 +63,6 @@ if __name__== "__main__":
 
     ]
 
-    print(service_api_list)
     for service_api in service_api_list:
-        correct_requests = quizas_quisiste_decir(sys.argv[1], service_api)
-        print(correct_requests)
+        correct_request = quizas_quisiste_decir(sys.argv[1], service_api)
+        print(correct_request)
