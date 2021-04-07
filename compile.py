fb05e59e8e1eb16d70659f5cc03fd41743f2cd63
josemibnf <40121100+josemibnf@users.noreply.github.com>
2021-04-07 13:23:48 +0200

    Update compile.py

--- compile.py
+++ compile.py
@@ -14,7 +14,7 @@ HYCACHE = "/home/hy/node/__hycache__/"
 REGISTRY = "/home/hy/node/__registry__/"
 
 # -- HASH FUNCTIONS --
-SHAKE_256 = lambda value: "" if value is None else hashlib.shake_256(value).hexdigest()
+SHAKE_256 = lambda value: "" if value is None else hashlib.shake_256(value).hexdigest(32)
 SHA3_256 = lambda value: "" if value is None else hashlib.sha3_256(value).hexdigest()
 
 # ALERT: Its not async.
