103cd0b61176d670d8ad3e935173c42a8042d947
josemibnf <40121100+josemibnf@users.noreply.github.com>
2021-04-09 18:46:44 +0200

    GATEWAY_INSTANCE with uri_slot element implemented.

--- gateway.py
+++ gateway.py
@@ -251,7 +251,10 @@ if __name__ == "__main__":
     uri = gateway_pb2.ipss__pb2.Instance.Uri()
     uri.ip = '172.17.0.1'
     uri.port = GATEWAY_PORT
-    GATEWAY_INSTANCE.uri_slot[GATEWAY_PORT] = uri
+    uri_slot = gatewa_pb2.ipss__pb2.Instance.Uri_Slot()
+    uri_slot.internal_port = GATEWAY_PORT
+    uri_slot.uri.append(uri)
+    GATEWAY_INSTANCE.uri_slot.append(uri_slot)
 
     # create a gRPC server
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=30))
