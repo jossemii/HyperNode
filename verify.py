6ee0d71629a24b69fc11024346502d9d5d02785d
josemibnf <josemi.bnf@gmail.com>
2021-04-17 21:00:47 +0200

    verify.py

--- verify.py
+++ verify.py
@@ -20,14 +20,11 @@ def prune_hashes_of_service(service: Service) -> Service:
 
     def recursive_prune(field: any) -> any:
         try:
-            field.ClearField('hash')
-        except ValueError: pass
-        try:
             for attribute in field.ListFields():
                 if attribute[0].number == 15: # The hash field is always on index 15.
-                    s.ClearField(attribute[0].name) # 'name' is the field's name on our serializer.
+                    field.ClearField(attribute[0].name) # 'name' is the field's name on our serializer.
                 else:
-                    recursive_prune(field=field)
+                    recursive_prune(field=attribute[1])
         except AttributeError: pass
 
 def get_service_hash(service: Service, hash_type: str) -> list:
