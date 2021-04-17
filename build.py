506608eb9cc2f31a60b3a54b2dadc8add44912a3
josemibnf <josemi.bnf@gmail.com>
2021-04-17 22:45:55 +0200

    build fixed

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
-from compile import SHA3_256, LOGGER
+from compile import LOGGER
+from verify import get_service_hash
 from subprocess import check_output, CalledProcessError
 
 
@@ -6,7 +7,7 @@ def verify():
     pass
 
 def build(service):
-    id = eval("SHA3_256")(service.SerializeFromString())
+    id = get_service_hash(service=service, hash_type='sha3-256')
     # it's locally?
     try:
         check_output('/usr/bin/docker inspect '+id)
@@ -19,9 +20,9 @@ if __name__ == "__main__":
     import gateway_pb2, sys
     id = sys.argv[1]
     with open("/home/hy/node/__registry__/"+id+".service", "rb") as file:
-        file = gateway_pb2.ServiceFile()
-        file.ParseFromString(file.read())
-        if eval("SHA3_256")(file.service.SerializeToString()) == id:
-            build(file.service)
+        service = gateway_pb2.ipss__pb2.Service()
+        service.ParseFromString(file.read())
+        if get_service_hash(service=service, hash_type='sha3-256') == id:
+            build(service=service)
         else:
             print('Error: asignacion de servicio erronea en el registro.')
