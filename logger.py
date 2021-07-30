50ae65fdb4ff0b6ec5fc2747fc046e615c4eb54a
josemibnf <josemi.bnf@gmail.com>
2021-07-30 12:36:52 +0200

    log

--- logger.py
+++ logger.py
@@ -1,3 +1,3 @@
 import logging
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
-LOGGER = lambda message: logging.getLogger(__name__).debug(message + '\n')
+LOGGER = lambda message: logging.getLogger(__name__).debug('\n' + message + '\n')
