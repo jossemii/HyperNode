0631eee23cb22335a160e8c694a0a04930314941
josemibnf <josemi.bnf@gmail.com>
2021-09-07 19:04:14 +0200

    ..

--- gateway.py
+++ gateway.py
@@ -235,7 +235,7 @@ def service_balancer(service: gateway_pb2.ipss__pb2.Service) -> gateway_pb2.ipss
                 ).cost
                 l.LOGGER('The cost could be ' + str(cost))
             except: l.LOGGER('Error taking the cost.')
-            l.LOGGER(str(cost) +  str(min_cost), str(cost and cost < min_cost))
+            l.LOGGER(str(cost) +  str(min_cost) + str(cost and cost < min_cost))
             if cost and cost < min_cost: 
                 min_cost = cost
                 best_peer = peer_instance
