f6d287774e39778f0c0af5ddb422e6e33652f10a
josemibnf <josemi.bnf@gmail.com>
2021-10-16 12:51:53 +0200

    df

--- gateway.py
+++ gateway.py
@@ -258,6 +258,7 @@ def service_balancer(service: celaut.Service, metadata: celaut.Any.Metadata) -> 
             weight = execution_cost(service = service, metadata = metadata)
         )
 
+        l.LOGGER('Local execution cost for the service is: ' + str(peers[None]))
         for peer in list(pymongo.MongoClient(
                         "mongodb://localhost:27017/"
                     )["mongo"]["peerInstances"].find()):
