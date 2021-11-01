8e95da4f8113c2a63d032aa536b1104236e11be3
josemibnf <josemi.bnf@gmail.com>
2021-11-01 19:07:40 +0100

    asd

--- compile.py
+++ compile.py
@@ -235,16 +235,15 @@ class Hyper:
                 )
 
     def save(self):
-        service_buffer = self.service.SerializeToString()
         self.metadata.complete = True
         self.metadata.hashtag.hash.extend(
             get_service_list_of_hashes(
-                service_buffer = service_buffer, 
+                service_buffer = self.service.SerializeToString(), 
                 metadata = self.metadata
             )
         )
         id = get_service_hex_main_hash(
-            service_buffer = service_buffer, 
+            service_buffer = self.service.SerializeToString(), 
             metadata = self.metadata
             )
         # Once service hashes are calculated, we prune the filesystem for save storage.
@@ -254,7 +253,7 @@ class Hyper:
             f.write(
                     celaut.Any(
                         metadata = self.metadata,
-                        value = service_buffer
+                        value = self.service.SerializeToString()
                     ).SerializeToString()
                 )
         return id
