7dcaac60833ec1ad6bfcad47d8f697a644592758
josemibnf <josemi.bnf@gmail.com>
2021-11-03 18:13:12 +0100

    asd

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-import utils, time, celaut_pb2
+import utils, time, celaut_pb2, gc
 
 start = time.time()
 buff = utils.read_file('__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d55fc95456')
 any = celaut_pb2.Any()
 any.ParseFromString(buff)
 print(len(buff), time.time() - start)
-time.sleep(10)
+gc.collect()
+
 start = time.time()
 print(
     len(utils.read_file('__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d55fc95456')), time.time() - start
