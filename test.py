34d8709732ce5ca24cc377518be9a105b3db025a
josemibnf <josemi.bnf@gmail.com>
2021-11-03 15:28:25 +0100

    ad

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 import utils
-len(utils.read_file('__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d55fc95456'))
+print(len(utils.read_file('__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d55fc95456')))
