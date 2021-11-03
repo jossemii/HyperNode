91bc0cfbdc6d26411282b1645750e4e979c717b6
josemibnf <josemi.bnf@gmail.com>
2021-11-03 19:05:05 +0100

    asd

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import utils, time, celaut_pb2, gc
+import utils, time, celaut_pb2
 
 start = time.time()
 buff = utils.read_file('__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d55fc95456')
@@ -6,3 +6,11 @@ any = celaut_pb2.Any()
 any.ParseFromString(buff)
 print(len(buff), time.time() - start)
 del buff
+print('del buff')
+
+utils.prevent_kill(len(any.value))
+service = celaut_pb2.Service()
+service.ParseFromString(any.value)
+service.container.ClearField('filesystem')
+
+print('do it.')
