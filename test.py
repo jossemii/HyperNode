1aed157f5be3f5e6aa8d0ec29603e050a5c8d270
josemibnf <josemi.bnf@gmail.com>
2021-11-04 12:21:24 +0100

    asd

--- test.py
+++ test.py
@@ -5,26 +5,26 @@ filename = '__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d
 
 def test():
     io = utils.IOBigData()
-    io.lock_ram(os.path.getsize(filename))
+    io.lock_ram(1.5*os.path.getsize(filename))
 
     any = celaut_pb2.Any()
     any.ParseFromString(
     utils.read_file(filename)
     )
 
-    io.lock_ram(len(any.value))
+    io.lock_ram(1.5*len(any.value))
     service = celaut_pb2.Service()
     service.ParseFromString(any.value)
 
     del any
-    io.unlock_ram(os.path.getsize(filename))
+    io.unlock_ram(1.5*os.path.getsize(filename))
 
 
     filesys_len = len(service.container.filesystem)
     service.container.ClearField('filesystem')
     print('filesystem clear')
 
-    io.unlock_ram(filesys_len)
+    io.unlock_ram(1.5*filesys_len)
     time.sleep(10)
     print('do it. ', io.get_ram_avaliable())
 
