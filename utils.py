f83dac64b5b6a3293044284da21cc6e6cdff28ff
josemibnf <josemi.bnf@gmail.com>
2021-11-04 12:17:43 +0100

    asd

--- utils.py
+++ utils.py
@@ -87,27 +87,11 @@ def get_network_name( ip_or_uri: str) -> str:
         except KeyError:
             continue
 
-
-# Thread-sage Singleton
-import threading
-class Singleton(type):
-  _instance = None
-  _lock = threading.Lock()
-
-  def __call__(cls, *args, **kwargs):
-    if not cls._instance:
-      with cls._lock:
-        # another thread could have created the instance
-        # before we acquired the lock. So check that the
-        # instance is still nonexistent.
-        if not cls._instance:
-          cls._instance = super().__call__(*args, **kwargs)
-    return cls._instance
-
 # I/O Big Data utils.
 import psutil, os.path
 from time import sleep
 from threading import Lock
+from singleton import Singleton
 
 def read_file(filename) -> bytes:
     def generator(filename):
