9e3c1e0fcebabc7ab23ca655fafd61c863f5d269
josemibnf <josemi.bnf@gmail.com>
2021-11-04 15:48:03 +0100

    asd

--- utils.py
+++ utils.py
@@ -123,9 +123,8 @@ class IOBigData(metaclass=Singleton):
 
     def __init__(self) -> None:
         print('Init object')
-        self.ram_pool = psutil.virtual_memory().available
         self.ram_locked = 0
-        self.get_ram_avaliable = lambda: self.ram_pool - self.ram_locked
+        self.get_ram_avaliable = lambda: psutil.virtual_memory().available - self.ram_locked
         self.amount_lock = Lock()
 
     def stats(self):
