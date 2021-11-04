0bc5bf61820e55f207f5a33b73b09643a46c10da
josemibnf <josemi.bnf@gmail.com>
2021-11-04 11:28:11 +0100

    asd

--- utils.py
+++ utils.py
@@ -94,14 +94,14 @@ class Singleton(type):
   _instance = None
   _lock = threading.Lock()
 
-  def __new__(cls, *args, **kwargs):
+  def __call__(cls, *args, **kwargs):
     if not cls._instance:
       with cls._lock:
         # another thread could have created the instance
         # before we acquired the lock. So check that the
         # instance is still nonexistent.
         if not cls._instance:
-          cls._instance = super(Singleton, cls).__new__(cls)
+          cls._instance = super().__call__(*args, **kwargs)
     return cls._instance
 
 # I/O Big Data utils.
