784ede177c3fdfc32edac15f1f98464725755f3e
josemibnf <josemi.bnf@gmail.com>
2021-11-04 16:38:09 +0100

    asd

--- test.py
+++ test.py
@@ -5,23 +5,12 @@ filename = '__registry__/16426da109eed68c89bf32bcbcab208649f01d608116f1dda15e12d
 
 def test():
     io = utils.IOBigData()
-    with io.lock(len = 4*os.path.getsize(filename)):
+    with io.lock(len = 2*os.path.getsize(filename)):
         any = celaut_pb2.Any()
         any.ParseFromString(
             utils.read_file(filename)
         )
         print('file readed')
-
-        service = celaut_pb2.Service()
-        service.ParseFromString(any.value)
-
-        del any
-
-        service.container.ClearField('filesystem')
-        print('filesystem clear')
-        print('do it. ', io.get_ram_avaliable())
-        print('We want ', service.api)
-        del service
 
 from threading import Thread
 import psutil
