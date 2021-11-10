2ae93f89da3c51a4a210072047dcc191f981d71a
josemibnf <josemi.bnf@gmail.com>
2021-11-10 22:56:57 +0100

    asd

---
+++ test.py
@@ -0,0 +1,26 @@
+import grpc, gateway_pb2_grpc
+from concurrent import futures
+import grpcbigbuffer
+import gateway_pb2
+
+
+class Gateway(gateway_pb2_grpc.Gateway):
+    def StartService(self, request_iterator, context):
+        parser = grpcbigbuffer.parse_from_buffer(request_iterator=request_iterator, message_field=gateway_pb2.TokenMessage)
+        while True:
+            r = next(parser)
+            print(r)
+            if type(r) is gateway_pb2.TokenMessage:
+                print(r.token)
+
+
+# create a gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+gateway_pb2_grpc.add_GatewayServicer_to_server(
+    Gateway(), server=server
+)
+
+server.add_insecure_port('[::]:' + str(8080))
+print('Starting gateway at port'+ str(8080))
+server.start()
+server.wait_for_termination()
