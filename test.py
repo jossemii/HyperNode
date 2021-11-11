8655acd37b2f1e4f9874ba7e03c587b31a573cdf
josemibnf <josemi.bnf@gmail.com>
2021-11-11 11:50:58 +0100

    asd

--- test.py
+++ test.py
@@ -6,15 +6,16 @@ import gateway_pb2
 
 class Gateway(gateway_pb2_grpc.Gateway):
     def StartService(self, request_iterator, context):
-        parser = grpcbigbuffer.parse_from_buffer(request_iterator=request_iterator, message_field=gateway_pb2.TokenMessage)
-        while True:
-            r = next(parser)
-            print(r)
-            if type(r) is gateway_pb2.TokenMessage:
-                for b in grpcbigbuffer.serialize_to_buffer(
-                    message_iterator=gateway_pb2.Instance(token=r.token)
-                ): yield b
-            print('ok')
+        try:
+            parser = grpcbigbuffer.parse_from_buffer(request_iterator=request_iterator, message_field=gateway_pb2.TokenMessage)
+            while True:
+                r = next(parser)
+                print(r)
+                if type(r) is gateway_pb2.TokenMessage:
+                    for b in grpcbigbuffer.serialize_to_buffer(
+                        message_iterator=gateway_pb2.Instance(token=r.token)
+                    ): yield b
+        except Exception as e: print(e)
 
 
 # create a gRPC server
