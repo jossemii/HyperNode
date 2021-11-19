03e9e4435553785728d2c2ccf410cf36e77f8571
josemibnf <josemi.bnf@gmail.com>
2021-11-19 19:55:25 +0100

    asd

--- grpcbigbuffer.py
+++ grpcbigbuffer.py
@@ -455,6 +455,7 @@ def serialize_to_buffer(
                 finally: signal.wait()
 
         for message in message_iterator:
+            print('message -> ', message)
             if type(message) is tuple:  # If is partitioned
                 yield buffer_pb2.Buffer(
                     head = buffer_pb2.Buffer.Head(
