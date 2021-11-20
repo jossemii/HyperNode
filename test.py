920010ba23af0a11e0fbb64f7e23db14af6d52b2
josemibnf <josemi.bnf@gmail.com>
2021-11-20 19:37:12 +0100

    asd

--- test.py
+++ test.py
@@ -20,10 +20,6 @@ class Gateway(gateway_pb2_grpc.Gateway):
                     complete  = True
                 )
                 for b in grpcbigbuffer.serialize_to_buffer(
-                    partitions_model = [
-                        buffer_pb2.Buffer.Head.Partition(index={1: buffer_pb2.Buffer.Head.Partition(), 2: buffer_pb2.Buffer.Head.Partition()}),
-                        buffer_pb2.Buffer.Head.Partition(index={3: buffer_pb2.Buffer.Head.Partition()}),
-                    ],
                     message_iterator=gateway_pb2.Instance(
                         token=r.token,
                         instance_meta=meta
