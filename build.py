10f8bb57827822c00ef94411e577d798cc8cc171
jossemii <josemi.bnf@gmail.com>
2022-02-11 14:44:52 +0100

    asd

--- build.py
+++ build.py
@@ -72,9 +72,11 @@ def build_container_from_definition(service: gateway_pb2.celaut__pb2.Service, me
 
     # Generate the symlinks.
     overlay_dir = check_output("docker inspect --format='{{ .GraphDriver.Data.UpperDir }}' "+id+'.docker', shell=True).decode('utf-8')[:-1]
+    l.LOGGER('Build process of '+ id + ': overlay dir '+str(overlay_dir))
     for symlink in symlinks: 
         if check_output('ln -s '+symlink.src+' '+symlink.dst[1:], shell=True, cwd=overlay_dir)[:2] == 'ln': break
 
+    l.LOGGER('Build process of '+ id + ': apply permissions.')
     # Apply permissions. # TODO check that is only own by the container root. https://programmer.ink/think/docker-security-container-resource-control-using-cgroups-mechanism.html
     run('find . -type d -exec chmod 777 {} \;', shell=True, cwd=overlay_dir)
     run('find . -type f -exec chmod 777 {} \;', shell=True, cwd=overlay_dir)
