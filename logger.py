50b9153f4c8bd33af27bcce3b03e58ad907b870a
josemiguel <josemiguel.avellana@m.e-tecnia.es>
2022-03-05 12:19:15 +0100

    adf

--- logger.py
+++ logger.py
@@ -1,4 +1,3 @@
 import logging, getpass
 logging.basicConfig(filename='/node/app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
-#LOGGER = lambda message: logging.getLogger(__name__).debug(message + '\n')
-LOGGER = lambda message: print(message + '\n')
+LOGGER = lambda message: logging.getLogger(__name__).debug(message + '\n')
