10a2555ad3eab1a910f18bf20e0600de4ed1ae0f
josemiguel <josemiguel.avellana@m.e-tecnia.es>
2022-03-05 16:21:27 +0100

    logg

--- logger.py
+++ logger.py
@@ -1,6 +1,5 @@
-import logging, utils
-
-USE_PRINT = utils.GET_ENV(env = 'USE_PRINT', default = False)
-
+import logging
+GET_ENV = lambda env, default: type(default)(os.environ.get(env)) if env in os.environ.keys() else default
+USE_PRINT = GET_ENV(env = 'USE_PRINT', default = False)
 logging.basicConfig(filename='/node/app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
 LOGGER = lambda message: print(message + '\n') if USE_PRINT else logging.getLogger(__name__).debug(message + '\n')
