6a6a7a0bdf0318641ea33ae3e9b91caa286b8049
josemiguel <josemiguel.avellana@m.e-tecnia.es>
2022-03-05 16:21:38 +0100

    adf

--- logger.py
+++ logger.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 GET_ENV = lambda env, default: type(default)(os.environ.get(env)) if env in os.environ.keys() else default
 USE_PRINT = GET_ENV(env = 'USE_PRINT', default = False)
 logging.basicConfig(filename='/node/app.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
