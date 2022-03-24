23c38363ec4ef33e0fc84b8f78675f000dc7cace
josemiguel <josemiguel.avellana@m.e-tecnia.es>
2022-03-24 21:19:26 +0100

    fda

--- build.py
+++ build.py
@@ -44,7 +44,7 @@ def build_container_from_definition(service_buffer: bytes, metadata: gateway_pb2
             )
 
     second_partition_dir = REGISTRY + id + '/p2'
-    l.LOGGER('Build process of '+ id + ': wait for unlock the memory.')
+    l.LOGGER('Build process of '+ id + ': wait for unlock the memory.' + str(len(service_buffer) + BUILD_CONTAINER_MEMORY_SIZE_FACTOR*os.path.getsize(second_partition_dir)))
     with iobigdata.mem_manager(len = len(service_buffer) + BUILD_CONTAINER_MEMORY_SIZE_FACTOR*os.path.getsize(second_partition_dir)):
         l.LOGGER('Build process of '+ id + ': go to load all the buffer.')
         service = gateway_pb2.celaut__pb2.Service()
