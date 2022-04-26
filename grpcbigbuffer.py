9d1a6e5cd2550ca34b4a8c7b6655911b3a208972
jossemii <josemiguel.avellana@m.e-tecnia.es>
2022-04-26 13:51:00 +0200

    fda

--- grpcbigbuffer.py
+++ grpcbigbuffer.py
@@ -375,8 +375,11 @@ def parse_from_buffer(
                     aux_object.CopyFrom(main_object)
                 print('\n Go to get the submessage.')
                 print('aux_object pre -> ', aux_object.ByteSize())
+                from time import sleep
+                sleep(10)
                 aux_object = get_submessage(partition = partition, obj = aux_object)
                 print('aux_object post -> ', aux_object.ByteSize() if hasattr(aux_object, 'SerializeToString') else len(aux_object))
+                sleep(10)
                 message_mode = partitions_message_mode[i]
                 if not message_mode:
                     filename = generate_random_dir()
