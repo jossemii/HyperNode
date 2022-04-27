57c7dab7151ab89f325a094d2aca0861a5103399
jossemii <josemiguel.avellana@m.e-tecnia.es>
2022-04-27 11:37:34 +0200

    fda

--- compile.py
+++ compile.py
@@ -283,6 +283,7 @@ class Hyper:
 def ok(path, aux_id, partitions_model = [buffer_pb2.Buffer.Head.Partition()]):
     Hyperfile = Hyper(path = path, aux_id = aux_id)
 
+    print('buffer len -> ', Hyperfile.buffer_len)
     with iobigdata.mem_manager(len = 1.5*Hyperfile.buffer_len):
         Hyperfile.parseContainer()
         Hyperfile.parseApi()
