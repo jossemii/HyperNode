f2db574b0d363cf9a072d8826328762dfada317c
jossemii <josemiguel.avellana@m.e-tecnia.es>
2022-05-01 20:40:51 +0200

    fda

--- gateway.py
+++ gateway.py
@@ -954,9 +954,7 @@ class Gateway(gateway_pb2_grpc.Gateway):
                 break
                 
         l.LOGGER('Execution cost for a service is requested, cost -> ' + str(cost))
-        if not cost: 
-            print('I dontve the service ', str(cost), not cost)
-            raise Exception("I dont've the service.")
+        if cost is None: raise Exception("I dont've the service.")
         for b in grpcbf.serialize_to_buffer(
             message_iterator = gateway_pb2.CostMessage(
                                 cost = cost + EXECUTION_BENEFIT
