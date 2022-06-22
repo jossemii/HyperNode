2e9a753c5e52965763f4909352cab09c9e76f33a
jossemii <josemi.bnf@gmail.com>
2022-06-22 02:32:26 +0200

    fda

--- gateway.py
+++ gateway.py
@@ -990,7 +990,7 @@ class Gateway(gateway_pb2_grpc.Gateway):
         ): yield b
 
 
-    def Payment(self, request_iterator, context):
+    def Payment(self, request_iterator, context):  # TODO
         l.LOGGER('Request for payment.')
         payment = next(grpcbf.parse_from_buffer(
             request_iterator = request_iterator
