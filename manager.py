2806a7757adcc8b6cdd8c9f38bfe3d0592ce1df6
jossemii <josemi.bnf@gmail.com>
2022-06-26 20:11:48 +0200

    fda

--- manager.py
+++ manager.py
@@ -340,7 +340,7 @@ def add_peer(
 def default_cost(
     father_ip: str = None
 ) -> int:
-    l.LOGGER('Default cost for '+father_ip)
+    l.LOGGER('Default cost for '+(father_ip if father_ip else 'local'))
     return ( __get_gas_amount_by_father_ip( father_ip = father_ip ) * int(DEFAULT_INITIAL_GAS_AMOUNT_FACTOR) ) if father_ip else int(DEFAULT_INTIAL_GAS_AMOUNT)
 
 def add_container(
