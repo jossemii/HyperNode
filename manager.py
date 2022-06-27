76892956fca1df54c53ea83a8c985950f3746b84
jossemii <josemi.bnf@gmail.com>
2022-06-27 15:36:42 +0200

    dafad

--- manager.py
+++ manager.py
@@ -77,7 +77,7 @@ deposits_on_other_peers = {}  # id: amount of gas -> the deposits in other peers
 container_cache_lock = threading.Lock()
 container_cache = {}  # ip_father:[dependencies]
 
-cache_service_perspective = {} # service_ip:token
+cache_service_perspective = {} # service_ip:(container_id or external_service_token)
 
 # internal token -> str( peer_ip##container_ip##container_id )   peer_ip se refiere a la direccion del servicio padre (que puede ser interno o no).
 # external token -> str( peer_ip##node_ip:node_port##his_token )
