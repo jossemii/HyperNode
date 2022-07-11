f97c262da0b5030e76aa98e729f51e2f8e420cf2
jossemii <josemi.bnf@gmail.com>
2022-07-11 21:57:57 +0200

    fda

--- utils.py
+++ utils.py
@@ -157,8 +157,7 @@ def get_ledger_and_contract_address_from_peer_id_and_ledger(contract_hash: bytes
 
     try:
         for peer in peers:
-            print('peer_id ', peer_id, peer['instance']['uriSlot'][0]['uri'][0])
-            if peer_id == peer['instance']['uriSlot'][0]['uri'][0]:  # TODO Cuando se use peer_id podra usar filter.
+            if peer_id == peer['instance']['uriSlot'][0]['uri'][0]['ip']:  # TODO Cuando se use peer_id podra usar filter.
                 if sha256(base64.b64decode(peer['instance']['api']['contractLedger'][0]['contract'])).digest() == contract_hash:
                     return peer['instance']['api']['contractLedger'][0]['ledger'], peer['instance']['api']['contractLedger'][0]['contractAddr']
     except Exception as e: print(str(e))
@@ -170,6 +169,6 @@ def get_own_token_from_peer_id(peer_id: str) -> str:
             )["mongo"]["peerInstances"].find())
 
     for peer in peers:
-        if peer_id == peer['instance']['uriSlot'][0]['uri'][0]:  # TODO Cuando se use peer_id podra usar filter.
+        if peer_id == peer['instance']['uriSlot'][0]['uri'][0]['ip']:  # TODO Cuando se use peer_id podra usar filter.
             return peer['token']
     raise Exception('No token found for peer: ' + str(peer_id))
