0af71b41842d9881bb518d27d91f84cf3d358377
jossemii <josemi.bnf@gmail.com>
2022-07-11 21:03:30 +0200

    fda

--- utils.py
+++ utils.py
@@ -156,10 +156,12 @@ def get_ledger_and_contract_address_from_peer_id_and_ledger(contract_hash: bytes
             )["mongo"]["peerInstances"].find())
 
     print('list of peers ', peers)
-    for peer in peers:
-        if peer_id != peer['instance']['uriSlot'][0]['uri'][0]:  # TODO Cuando se use peer_id podra usar filter.
-            if sha256(base64.b64decode(peer['instance']['api']['contractLedger'][0]['contract'])).digest() == contract_hash:
-                return peer['instance']['api']['contractLedger'][0]['ledger'], peer['instance']['api']['contractLedger'][0]['contractAddr']
+    try:
+        for peer in peers:
+            if peer_id != peer['instance']['uriSlot'][0]['uri'][0]:  # TODO Cuando se use peer_id podra usar filter.
+                if sha256(base64.b64decode(peer['instance']['api']['contractLedger'][0]['contract'])).digest() == contract_hash:
+                    return peer['instance']['api']['contractLedger'][0]['ledger'], peer['instance']['api']['contractLedger'][0]['contractAddr']
+    except Exception as e: print(str(e))
     raise Exception('No ledger found for contract: ' + str(contract_hash))
 
 def get_own_token_from_peer_id(peer_id: str) -> str:
