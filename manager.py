e973a9d82bb516e13cafc7811483919875a1ef9c
jossemii <josemi.bnf@gmail.com>
2022-07-14 20:26:20 +0200

    fda

--- manager.py
+++ manager.py
@@ -323,7 +323,11 @@ def __increase_deposit_on_peer(peer_id: str, amount: int) -> bool:
         with deposits_on_other_peers_lock:
             deposits_on_other_peers[peer_id] = deposits_on_other_peers[peer_id] + amount if peer_id in deposits_on_other_peers else amount
         return True
-    return False
+    else:
+        if peer_id not in deposits_on_other_peers:
+            with deposits_on_other_peers_lock:
+                deposits_on_other_peers[peer_id] = 0
+        return False
 
 def __check_payment_process( amount: int, ledger: str, token: str, contract: bytes, contract_addr: string) -> bool:
     l.LOGGER('Check payment process to '+token+' of '+str(amount))
