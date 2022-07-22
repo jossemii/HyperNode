df71d5f338b3f9fb2658191a79abd5b804c08d25
jossemii <josemi.bnf@gmail.com>
2022-07-22 20:16:12 +0200

    fad

--- gateway.py
+++ gateway.py
@@ -886,7 +886,6 @@ class Gateway(gateway_pb2_grpc.Gateway):
 
     
     def GetMetrics(self, request_iterator, context):
-        l.LOGGER('Request for metrics.')
         for b in grpcbf.serialize_to_buffer(
             message_iterator = get_metrics(
                 token = next(grpcbf.parse_from_buffer(
