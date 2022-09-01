a1944ba1886a8aaf3d9aff1019db32f437d00cf5
jossemii <josemi.bnf@gmail.com>
2022-09-01 19:56:51 +0200

    fda

--- logger.py
+++ logger.py
@@ -2,6 +2,6 @@ import logging, os
 GET_ENV = lambda env, default: ( type(default)(os.environ.get(env)) if type(default)!=bool else os.environ.get(env) in ['True', 'true', 'T', 't'] ) if env in os.environ.keys() else default
 USE_PRINT = GET_ENV(env = 'USE_PRINT', default = False)
 
-logging.basicConfig(filename='/node/hyapp.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
+logging.basicConfig(filename='/node/hyapp.log', level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
 LOGGER = (lambda message: print(message + '\n')) if USE_PRINT \
-     else (lambda message: logging.getLogger(__name__).debug(message + '\n'));     LOGGER('USE PRINT -> '+ str(USE_PRINT))
+     else (lambda message: logging.getLogger(__name__).info(message + '\n'));     LOGGER('USE PRINT -> '+ str(USE_PRINT))
