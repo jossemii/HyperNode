6e536d0f2cab67a9cccc606a538e99d74c898fc5
jossemii <josemi.bnf@gmail.com>
2022-09-19 22:23:17 +0200

    fda

--- duplicate_grabber.py
+++ duplicate_grabber.py
@@ -1,16 +1,32 @@
 from multiprocessing import Lock
-from typing import Any, Dict, Generator, List, Tuple
+from typing import Any, Dict, Generator, List
 import uuid
 import celaut_pb2 as celaut
 from utils import Singleton
-from threading import Event
+from threading import Barrier, Event, Lock
 
+class Session:
+    
+    def __init__(self) -> None:
+        self.event = Event()
+        self.value = None
+        self.barrier_count_lock = Lock()
+        self.barrier_count = 0
+
+    def wait(self):
+        with self.barrier_count_lock:
+            self.barrier_count += 1
+        self.event.wait()
+
+    def set(self):
+        self.barrier = Barrier(self.barrier_count)
+        self.event.set()
 
 class DuplicateGrabber(metaclass=Singleton):
 
     def __init__(self):
         self.hashes: Dict[celaut.Any.Metadata.HashTag.Hash, str] = {}
-        self.sessions: Dict[str: Tuple[Event, Any] ] = {}
+        self.sessions: Dict[str: Session ] = {}
         self.lock = Lock()
 
     def next(self,
@@ -20,17 +36,19 @@ class DuplicateGrabber(metaclass=Singleton):
 
         if True in [hashes in self.hashes.keys()]:
             session: str = self.hashes[hashes[0]]
-            self.sessions[session][0].wait()
-            if self.sessions[session][1] is None: raise Exception('Session not ended.')
-            return self.sessions[session][1]
+            self.sessions[session].wait()
+            if self.sessions[session].value is None: raise Exception('Session not ended.')
+            value =  self.sessions[session].value
+            self.sessions[session].barrier.wait()
+            return value
 
         else:
             session = uuid()
             for hash in hashes:
                 self.hashes[hash] = session
-            self.sessions[session] = (Event(), None)
+            self.sessions[session] = Session()
 
             result = next(generator)
-            self.sessions[session][1] = result
-            self.sessions[session][0].set()
+            self.sessions[session].value = result
+            self.sessions[session].set()
             return result
