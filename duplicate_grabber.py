c48dbb11218f2675c1023a5ed02866f122f503cf
jossemii <josemi.bnf@gmail.com>
2022-09-19 22:01:35 +0200

    fda

--- duplicate_grabber.py
+++ duplicate_grabber.py
@@ -3,33 +3,34 @@ from typing import Any, Dict, Generator, List, Tuple
 import uuid
 import celaut_pb2 as celaut
 from utils import Singleton
+from threading import Event
 
 
 class DuplicateGrabber(metaclass=Singleton):
 
     def __init__(self):
         self.hashes: Dict[celaut.Any.Metadata.HashTag.Hash, str] = {}
-        self.sessions: Dict[str: Tuple[, Any] ] = {}
+        self.sessions: Dict[str: Tuple[Event, Any] ] = {}
         self.lock = Lock()
 
     def next(self,
-        hashes: List[celaut.Any.Metadata.HashTag.Hash], 
+        hashes: List[celaut.Any.Metadata.HashTag.Hash],
         generator: Generator
     ) -> Any:
 
         if True in [hashes in self.hashes.keys()]:
             session: str = self.hashes[hashes[0]]
-            if self.sessions[session][0]:
-                return self.sessions[session][1]
+            self.sessions[session][0].wait()
+            if self.sessions[session][1] is None: raise Exception('Session not ended.')
+            return self.sessions[session][1]
 
         else:
-            
             session = uuid()
             for hash in hashes:
                 self.hashes[hash] = session
-            self.sessions[session] = (signal(), None)
+            self.sessions[session] = (Event(), None)
 
             result = next(generator)
             self.sessions[session][1] = result
-            self.sessions[session][0].
+            self.sessions[session][0].set()
             return result
