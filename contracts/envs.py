5b08f8eeaafc13ecb9507996b34d368cb582b331
jossemii <josemi.bnf@gmail.com>
2022-10-16 17:46:41 +0200

    asd

--- contracts/envs.py
+++ contracts/envs.py
@@ -1,11 +1,11 @@
-from types import LambdaType
-
+from types import FunctionType
 from typing import Dict
 
 from vyper_gas_deposit_contract import interface as vyper_gdc
 
-PAYMENT_PROCESS_VALIDATORS: Dict[bytes, LambdaType] = {
-    vyper_gdc.CONTRACT_HASH: vyper_gdc.payment_process_validator}  # contract_hash:  lambda peer_id, tx_id, amount -> bool,
-AVAILABLE_PAYMENT_PROCESS: Dict[bytes, LambdaType] = {
+PAYMENT_PROCESS_VALIDATORS: Dict[bytes, FunctionType] = {
+vyper_gdc.CONTRACT_HASH: vyper_gdc.payment_process_validator}  # contract_hash:  lambda peer_id, tx_id, amount -> bool,
+
+AVAILABLE_PAYMENT_PROCESS: Dict[bytes, FunctionType] = {
     vyper_gdc.CONTRACT_HASH: vyper_gdc.process_payment}  # contract_hash:   lambda amount, peer_id -> tx_id,
 
