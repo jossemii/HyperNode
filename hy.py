ff62e9da58053083dc96d5683f9246b7aa6fd58e
jossemii <josemi.bnf@gmail.com>
2022-10-16 18:27:34 +0200

    asd

--- hy.py
+++ hy.py
@@ -77,15 +77,3 @@ if __name__ == "__main__":
             service_id = id
         )
     )
-
-def print_cache():
-    from src.manager.system_cache import SystemCache
-
-    system_cache = SystemCache()
-    print('\nSystem cache -> ', system_cache.system_cache)
-    print('\nClients -> ', system_cache.clients)
-    print('\nTotal deposited on other peers -> ', system_cache.total_deposited_on_other_peers)
-    print('\nClietns on other peers -> ', system_cache.clients_on_other_peers)
-    print('\nContainer cache -> ', system_cache.container_cache)
-    print('\nCache service perspective -> ', system_cache.cache_service_perspective)
-    print('\nExternal token map -> ', system_cache.external_token_hash_map)
