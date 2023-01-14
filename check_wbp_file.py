fe13d47f3f8dabbb351fa7c12f5c27425e9d4d56
Josemi <josemiguel.avellana@m.e-tecnia.es>
2023-01-14 17:39:02 +0100

    sad

---
+++ check_wbp_file.py
@@ -0,0 +1,16 @@
+from grpcbigbuffer.client import send_message
+from hashlib import sha3_256
+from protos.gateway_pb2 import ServiceWithMeta
+
+h = sha3_256()
+
+service = ServiceWithMeta()
+
+service.ParseFromString(
+    open('__cache__/grpcbigbuffer/12204466/wbp.bin').read()
+)
+
+for i in send_message(service.service):
+    h.update(i.chunk)
+
+print(h.hexdigest())
