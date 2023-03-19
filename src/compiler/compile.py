0e547cba35f0dc8d7a4b1fbe6d43276a4e183658
josemii <josemiguel.avellana@m.e-tecnia.es>
2023-03-19 19:17:56 +0100

    compile.py sin metadata for service.

--- src/compiler/compile.py
+++ src/compiler/compile.py
@@ -304,25 +304,28 @@ class Hyper:
                 blocks=self.blocks
             )
             service_id: str = codecs.encode(bytes_id, 'hex').decode('utf-8')
+            """
             self.metadata.hashtag.hash.extend(
                 calculate_hashes_by_stream(
                     value=grpcbb.read_multiblock_directory(
                         directory=directory,
-                        delete_directory=False,
+                        delete_directory=True,
                         ignore_blocks=True
                     )
                 )
             )
+            
 
             if service_id != get_service_hex_main_hash(metadata=self.metadata):
                 raise Exception('Compiler error obtaining the service id -> ' + service_id +
                                 ' ' + get_service_hex_main_hash(metadata=self.metadata),
                                 type(service_id), type(get_service_hex_main_hash(metadata=self.metadata)))
+            """
 
             # Generate the service with metadata.
             content_id, service_with_meta = block_builder.build_multiblock(
                 pf_object_with_block_pointers=compile_pb2.ServiceWithMeta(
-                        metadata=self.metadata,
+                        # metadata=self.metadata,
                         service=self.service
                     ),
                 blocks=self.blocks
