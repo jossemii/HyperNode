bfa4eac66ce27d3a7dfc280b31b3d04e4de2722c
josemii <josemiguel.avellana@m.e-tecnia.es>
2023-03-26 12:47:33 +0200

    resources_manager.py reformat file

--- src/manager/resources_manager.py
+++ src/manager/resources_manager.py
@@ -7,43 +7,45 @@ import threading
 
 
 class Singleton(type):
-  _instances = {}
-  _lock = threading.Lock()
+    _instances = {}
+    _lock = threading.Lock()
 
-  def __call__(cls, *args, **kwargs):
-    if cls not in cls._instances:
-      with cls._lock:
-        # another thread could have created the instance
-        # before we acquired the lock. So check that the
-        # instance is still nonexistent.
+    def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-          cls._instances[cls] = super().__call__(*args, **kwargs)
-    return cls._instances[cls]
+            with cls._lock:
+                # another thread could have created the instance
+                # before we acquired the lock. So check that the
+                # instance is still nonexistent.
+                if cls not in cls._instances:
+                    cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 
 mem_manager = lambda len: IOBigData().lock(len=len)
-class IOBigData(metaclass=Singleton):
 
+
+class IOBigData(metaclass=Singleton):
     class RamLocker(object):
         def __init__(self, len, iobd):
             self.len = len
             self.iobd = iobd
 
         def __enter__(self):
-            self.iobd.lock_ram(ram_amount = self.len)
+            self.iobd.lock_ram(ram_amount=self.len)
             return self
 
         def unlock(self, amount: int):
-            self.iobd.unlock_ram(ram_amount = amount)
+            self.iobd.unlock_ram(ram_amount=amount)
             self.len -= amount
 
         def __exit__(self, type, value, traceback):
-            self.iobd.unlock_ram(ram_amount = self.len)
+            self.iobd.unlock_ram(ram_amount=self.len)
             gc.collect()
 
-    def __init__(self, 
-            log = lambda message: print(message),
-            ram_pool_method = None
-        ) -> None:
+    def __init__(self,
+                 log=lambda message: print(message),
+                 ram_pool_method=None
+                 ) -> None:
 
         self.ram_pool = ram_pool_method
 
@@ -51,13 +53,13 @@ class IOBigData(metaclass=Singleton):
         self.ram_locked = 0
         self.get_ram_avaliable = lambda: self.ram_pool() - self.ram_locked
         self.amount_lock = Lock()
-        
+
         self.wait = []
         self.wait_lock = Lock()
 
     # General methods.
 
-    def set_log(self, log = lambda message: print(message)) -> None:
+    def set_log(self, log=lambda message: print(message)) -> None:
         self.log = log
 
     @staticmethod
@@ -71,19 +73,18 @@ class IOBigData(metaclass=Singleton):
             p = math.pow(1024, i)
             s = round(size_bytes / p, 2)
             return "%s %s" % (s, size_name[i])
-        except ValueError: return "%s %s" % (size_bytes, size_name[0])
+        except ValueError:
+            return "%s %s" % (size_bytes, size_name[0])
 
     def __stats(self, message: str, comments: bool = False):
         if comments:
             with self.amount_lock:
-                self.log('\n--------- '+message+' -------------')
-                self.log('RAM POOL       -> '+ IOBigData.convert_size(self.ram_pool()))
-                self.log('RAM LOCKED     -> '+ IOBigData.convert_size(self.ram_locked))
-                self.log('RAM AVALIABLE  -> '+ IOBigData.convert_size(self.get_ram_avaliable()))
-                self.log('RAM WAITING    -> '+ IOBigData.convert_size(sum(self.wait)))
+                self.log('\n--------- ' + message + ' -------------')
+                self.log('RAM POOL       -> ' + IOBigData.convert_size(self.ram_pool()))
+                self.log('RAM LOCKED     -> ' + IOBigData.convert_size(self.ram_locked))
+                self.log('RAM AVALIABLE  -> ' + IOBigData.convert_size(self.get_ram_avaliable()))
+                self.log('RAM WAITING    -> ' + IOBigData.convert_size(sum(self.wait)))
                 self.log('-----------------------------------------\n')
-
-
 
     # Gas manager methods.
     def __push_wait_list(self, l: int):
@@ -94,11 +95,10 @@ class IOBigData(metaclass=Singleton):
         with self.wait_lock:
             self.wait.remove(l)
 
-
     # Manage resources methods.
 
     def lock(self, len):
-        return self.RamLocker(len = len, iobd = self)
+        return self.RamLocker(len=len, iobd=self)
 
     def lock_ram(self, ram_amount: int, wait: bool = True):
         self.__stats('want lock ' + IOBigData.convert_size(ram_amount))
@@ -106,9 +106,9 @@ class IOBigData(metaclass=Singleton):
         while True:
             self.__stats('go to lock ' + IOBigData.convert_size(ram_amount))
             if wait:
-                self.wait_to_prevent_kill(len = ram_amount)
+                self.wait_to_prevent_kill(len=ram_amount)
 
-            elif not self.prevent_kill(len = ram_amount):
+            elif not self.prevent_kill(len=ram_amount):
                 self.__pop_wait_list(l=ram_amount)
                 raise Exception
 
@@ -137,7 +137,7 @@ class IOBigData(metaclass=Singleton):
 
     def wait_to_prevent_kill(self, len: int) -> None:
         while True:
-            if not self.prevent_kill(len = len):
+            if not self.prevent_kill(len=len):
                 sleep(1)
             else:
                 return
