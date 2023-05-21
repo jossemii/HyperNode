885b298a81f70e66781b9176592ecff98707403c
jossemii <josemi.bnf@gmail.com>
2023-05-21 19:14:17 +0200

    add commands

---
+++ commands/check_ram.py
@@ -0,0 +1,5 @@
+import psutil
+from time import sleep
+while True:
+    print('RAM memory % used:', psutil.virtual_memory()[2], ' -- ', psutil.virtual_memory())
+    sleep(1)
---
+++ commands/containers.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+import docker
+from tabulate import tabulate
+
+# Crear una instancia del cliente de Docker
+client = docker.from_env()
+
+while True:
+    # Borrar la consola
+    os.system('clear')
+
+    # Obtener la lista de contenedores en ejecución
+    contenedores = client.containers.list()
+
+    # Crear una lista para almacenar los datos de cada contenedor
+    datos_contenedores = []
+
+    # Recorrer los contenedores y obtener los datos relevantes
+    for contenedor in contenedores:
+        servicio = contenedor.image.tags[0]
+        creado = contenedor.attrs['Created']
+        puertos = contenedor.attrs['HostConfig']['PortBindings']
+        nombre = contenedor.name
+
+        # Agregar los datos a la lista
+        datos_contenedores.append([servicio, creado, puertos, nombre])
+
+    # Mostrar la tabla de datos
+    headers = ['SERVICE', 'CREATED', 'PORTS', 'NAMES']
+    print(tabulate(datos_contenedores, headers=headers, tablefmt="plain"))
+
+    # Esperar dos segundos antes de refrescar
+    time.sleep(2)
