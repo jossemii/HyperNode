c7af8e129eea10ad23719bb70876517525d6bbcc
jossemii <josemi.bnf@gmail.com>
2023-05-30 19:20:39 +0200

    ...

--- commands/peers.py
+++ commands/peers.py
@@ -1,26 +1,27 @@
 import os
 import time
-
 from tabulate import tabulate
 
 from src.utils.utils import peers_id_iterator
 
-while True:
-    # Borrar la consola
-    os.system('clear')
 
-    # Crear una lista para almacenar los datos de cada peer
-    datos_peeres = []
+def peers():
+    while True:
+        # Borrar la consola
+        os.system('clear')
 
-    # Recorrer los peeres y obtener los datos relevantes
-    for peer in peers_id_iterator():
+        # Crear una lista para almacenar los datos de cada peer
+        datos_peeres = []
 
-        # Agregar los datos a la lista
-        datos_peeres.append([peer])
+        # Recorrer los peeres y obtener los datos relevantes
+        for peer in peers_id_iterator():
 
-    # Mostrar la tabla de datos
-    headers = ['PAR']
-    print(tabulate(datos_peeres, headers=headers, tablefmt="plain"))
+            # Agregar los datos a la lista
+            datos_peeres.append([peer])
 
-    # Esperar dos segundos antes de refrescar
-    time.sleep(2)
+        # Mostrar la tabla de datos
+        headers = ['PAR']
+        print(tabulate(datos_peeres, headers=headers, tablefmt="plain"))
+
+        # Esperar dos segundos antes de refrescar
+        time.sleep(2)
