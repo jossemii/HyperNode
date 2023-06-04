03c363c17054a87121f6d39d8b2d4c53a626394a
jossemii <josemi.bnf@gmail.com>
2023-06-04 18:44:25 +0200

    test public key on command.

--- commands/contracts.py
+++ commands/contracts.py
@@ -1,13 +1,20 @@
 from typing import Generator, List
-
 from commands.__interface import command
 from src.utils.utils import get_ledgers
+from web3.auto import w3
+import binascii
 
 
 def generator() -> Generator[List[str], None, None]:
     for ledger_id, private_key in get_ledgers():
-        yield [ledger_id, private_key]
+        yield [
+            ledger_id,
+            binascii.hexlify(
+                w3.eth.account.privateKeyToAccount(private_key).public_key.to_bytes()
+            ).decode('utf-8'),
+            private_key
+        ]
 
 
 def contracts():
-    command(f=generator, headers=['LEDGER', 'PRIVATE KEY'])
+    command(f=generator, headers=['LEDGER', 'PUBLIC KEY', 'PRIVATE KEY'])
