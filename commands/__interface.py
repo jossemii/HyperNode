62b9e51f928576acd53b0301f6fc9148f41702ac
jossemii <josemi.bnf@gmail.com>
2023-06-06 19:19:06 +0200

    ...

--- commands/__interface.py
+++ commands/__interface.py
@@ -5,7 +5,7 @@ from tabulate import tabulate
 
 
 def table_command(f: Callable, headers: List[str], stream: bool = True, sleep_time: int = 2):
-    while stream:
+    while True:
         # Clear the console
         if stream:
             os.system('clear')
