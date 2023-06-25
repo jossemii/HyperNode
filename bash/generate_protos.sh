26381a62c98901a7a6597f3a91ffda836feed7c4
jossemii <josemi.bnf@gmail.com>
2023-06-25 15:12:24 +0200

    generate_protos.sh

--- bash/generate_protos.sh
+++ bash/generate_protos.sh
@@ -1,6 +1,6 @@
 # celaut
-python3 -m grpc_tools.protoc -I. --python_out=. celaut.proto --experimental_allow_proto3_optional &&
+python3 -m grpc_tools.protoc -I. --python_out=. protos/celaut.proto --experimental_allow_proto3_optional &&
 # gateway
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. gateway.proto --experimental_allow_proto3_optional &&
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. protos/gateway.proto --experimental_allow_proto3_optional &&
 # compile
-python3 -m grpc_tools.protoc -I. --python_out=. compile.proto --experimental_allow_proto3_optional 
+python3 -m grpc_tools.protoc -I. --python_out=. protos/compile.proto --experimental_allow_proto3_optional
