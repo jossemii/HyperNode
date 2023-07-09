5b8303fe174590eb68455a04c7961cd3d155981f
jossemii <josemi.bnf@gmail.com>
2023-07-09 15:39:25 +0200

    ...

--- contracts/eth_main/envs.py
+++ contracts/eth_main/envs.py
@@ -5,5 +5,3 @@ ETH_LEDGER = "fuji"
 PARITY_FACTOR = 100
 # Factor de paridad entre el valor de gas (representación de los recursos del nodo)
 # y el activo de la red de procesamiento de pagos.
-
-CONTRACT_ADDRESS = "0x6639fdB1eb6f0D42577c73fB6807ee15B1cc1784" # Contrato comodín, si el despliegue no funciona.
