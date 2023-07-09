1018c8346ab978c6a967156bf8d735002b35c57c
jossemii <josemi.bnf@gmail.com>
2023-07-09 15:25:53 +0200

    ...

--- contracts/eth_main/envs.py
+++ contracts/eth_main/envs.py
@@ -1,5 +1,9 @@
 ETH_PROVIDER = "https://data-seed-prebsc-1-s1.bnbchain.org:8545"
-ETH_LEDGER = "binance"
+
+ETH_LEDGER = "fuji"
+
 PARITY_FACTOR = 100
 # Factor de paridad entre el valor de gas (representación de los recursos del nodo)
 # y el activo de la red de procesamiento de pagos.
+
+CONTRACT_ADDRESS = "0x6639fdB1eb6f0D42577c73fB6807ee15B1cc1784" # Contrato comodín, si el despliegue no funciona.
