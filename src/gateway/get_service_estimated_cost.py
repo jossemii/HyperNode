a6f4e43092b7ac079704b6329cad6587bfb6262b
jossemii <josemi.bnf@gmail.com>
2023-08-02 19:36:26 +0200

    GetServiceEstimatedCost instead of a function.

---
+++ src/gateway/get_service_estimated_cost.py
@@ -0,0 +1,53 @@
+from typing import Optional, Generator
+
+from grpcbigbuffer import client as grpcbf, buffer_pb2
+
+from protos import gateway_pb2
+from src.builder import build
+from src.gateway.service_iterable import ServiceIterable
+from src.manager.manager import execution_cost, default_initial_cost
+from src.utils import logger as l
+from src.utils.env import GAS_COST_FACTOR
+from src.utils.utils import from_gas_amount, get_only_the_ip_from_context, to_gas_amount
+
+
+class GetServiceEstimatedCostIterable(ServiceIterable):
+    # TODO check cost in other peers (use RecursionGuard to prevent infinite loops).
+
+    cost: Optional[int] = None
+
+    def start(self):
+        l.LOGGER('Request for the cost of a service.')
+
+    def generate(self) -> Generator[buffer_pb2.Buffer, None, None]:
+        try:
+            self.cost = execution_cost(
+                metadata=self.metadata
+            ) * GAS_COST_FACTOR
+            # break
+        except build.UnsupportedArchitectureException as e:
+            raise e
+        except Exception as e:
+            yield gateway_pb2.buffer__pb2.Buffer(signal=True)
+            # continue
+
+    def final(self):
+        initial_service_cost = from_gas_amount(self.initial_gas_amount)
+        if not initial_service_cost:
+            initial_service_cost: int = default_initial_cost(
+                father_id=self.client_id if self.client_id \
+                    else get_only_the_ip_from_context(context_peer=self.context.peer())
+            )
+        cost: int = self.cost + initial_service_cost if self.cost else initial_service_cost
+        l.LOGGER('Execution cost for a service is requested, cost -> ' + str(cost) + ' with benefit ' + str(0))
+        if cost is None:
+            raise Exception("I dont've the service.")
+        for b in grpcbf.serialize_to_buffer(
+                message_iterator=gateway_pb2.EstimatedCost(
+                    cost=to_gas_amount(cost),
+                    variance=0  # TODO dynamic variance.
+                ),
+                indices=gateway_pb2.EstimatedCost
+        ): yield b
+
+
--- src/gateway/server.py
+++ src/gateway/server.py
@@ -1,24 +1,30 @@
-import os
-
 from grpcbigbuffer import client as grpcbf
 
-from protos import celaut_pb2 as celaut
 from protos import gateway_pb2_grpc, gateway_pb2
-from protos.gateway_pb2_grpcbf import StartService_input_indices, \
-    StartService_input_message_mode
-from src.builder import build
 from src.compiler.compile import compile_zip
-from src.gateway.start_service import start_service, get_from_registry, StartServiceIterable
-from src.gateway.utils import save_service, generate_gateway_instance
+from src.gateway.get_service_estimated_cost import GetServiceEstimatedCostIterable
+from src.gateway.start_service import StartServiceIterable
+from src.gateway.utils import generate_gateway_instance
 from src.manager.manager import prune_container, generate_client, get_token_by_uri, spend_gas, \
-    container_modify_system_params, get_sysresources, \
-    execution_cost, default_initial_cost
+    container_modify_system_params, get_sysresources
 from src.manager.metrics import get_metrics
 from src.payment_system.payment_process import validate_payment_process
 from src.utils import logger as l
-from src.utils.env import DENEGATE_COST_REQUEST_IF_DONT_VE_THE_HASH, SHA3_256_ID, \
-    MODIFY_SERVICE_SYSTEM_RESOURCES_COST, GAS_COST_FACTOR, REGISTRY
-from src.utils.tools.duplicate_grabber import DuplicateGrabber
+from src.utils.env import MODIFY_SERVICE_SYSTEM_RESOURCES_COST, GAS_COST_FACTOR
+from src.utils.utils import from_gas_amount, get_only_the_ip_from_context, to_gas_amount, get_network_name
+from grpcbigbuffer import client as grpcbf
+
+from protos import gateway_pb2_grpc, gateway_pb2
+from src.compiler.compile import compile_zip
+from src.gateway.get_service_estimated_cost import GetServiceEstimatedCostIterable
+from src.gateway.start_service import StartServiceIterable
+from src.gateway.utils import generate_gateway_instance
+from src.manager.manager import prune_container, generate_client, get_token_by_uri, spend_gas, \
+    container_modify_system_params, get_sysresources
+from src.manager.metrics import get_metrics
+from src.payment_system.payment_process import validate_payment_process
+from src.utils import logger as l
+from src.utils.env import MODIFY_SERVICE_SYSTEM_RESOURCES_COST, GAS_COST_FACTOR
 from src.utils.utils import from_gas_amount, get_only_the_ip_from_context, to_gas_amount, get_network_name
 
 
@@ -109,123 +115,7 @@ class Gateway(gateway_pb2_grpc.Gateway):
 
     # Estimacion de coste de ejecución de un servicio con la cantidad de gas por defecto.
     def GetServiceEstimatedCost(self, request_iterator, context, **kwargs):
-        # TODO check cost in other peers (use RecursionGuard to prevent infinite loops).
-
-        l.LOGGER('Request for the cost of a service.')
-        parse_iterator = grpcbf.parse_from_buffer(
-            request_iterator=request_iterator,
-            indices=StartService_input_indices,
-            partitions_message_mode=StartService_input_message_mode
-        )
-
-        client_id = None
-        recursion_guard_token = None
-        initial_service_cost = None
-        cost = None
-        initial_service_cost = None
-        service_hash = None
-        while True:
-            try:
-                r = next(parse_iterator)
-            except StopIteration:
-                break
-
-            if type(r) is gateway_pb2.Client:
-                client_id = r.client_id
-                continue
-
-            if type(r) is gateway_pb2.RecursionGuard:
-                recursion_guard_token = r.token
-                continue
-
-            if type(r) is gateway_pb2.HashWithConfig:
-                if r.HasField('initial_gas_amount'):
-                    initial_service_cost = from_gas_amount(r.initial_gas_amount)
-                service_hash: celaut.Any.Metadata.HashTag.Hash = r.hash
-                r = service_hash
-
-            if type(r) is celaut.Any.Metadata.HashTag.Hash and SHA3_256_ID == r.type:
-                if r.value.hex() in [s for s in os.listdir(REGISTRY)]:
-                    yield gateway_pb2.buffer__pb2.Buffer(signal=True)
-                    try:
-                        service_with_meta: gateway_pb2.ServiceWithMeta = get_from_registry(
-                            service_hash=r.value.hex()
-                        )
-                        cost = execution_cost(
-                            metadata=service_with_meta.metadata
-                        ) * GAS_COST_FACTOR
-                        break
-                    except build.UnsupportedArchitectureException as e:
-                        raise e
-                    except Exception as e:
-                        yield gateway_pb2.buffer__pb2.Buffer(signal=True)
-                        continue
-                elif DENEGATE_COST_REQUEST_IF_DONT_VE_THE_HASH:
-                    raise Exception("I dont've the service.")
-
-            if r is gateway_pb2.ServiceWithMeta:
-                if DENEGATE_COST_REQUEST_IF_DONT_VE_THE_HASH: raise Exception("I dont've the service.")
-                service_with_meta_dir, is_primary = DuplicateGrabber().next(
-                    hashes=[service_hash],
-                    generator=parse_iterator
-                )
-
-                save_service(
-                    service_dir=service_with_meta_dir,
-                    service_hash=service_hash.value.hex()
-                )
-
-                try:
-                    service_with_meta: gateway_pb2.ServiceWithMeta = get_from_registry(
-                        service_hash=service_hash.value.hex()
-                    )
-                    cost: int = execution_cost(
-                        metadata=service_with_meta.metadata
-                    ) * GAS_COST_FACTOR
-                except build.UnsupportedArchitectureException as e:
-                    raise e
-                break
-
-            if r is gateway_pb2.ServiceWithConfig:
-                print('SERVICE WITH CONFIG NOT SUPPORTED')
-                raise Exception('SERVICE WITH CONFIG NOT SUPPORTED')
-                """"
-                    if DENEGATE_COST_REQUEST_IF_DONT_VE_THE_HASH: raise Exception("I dont've the service.")
-                    service_with_config = next(parse_iterator)
-                    initial_service_cost: int = from_gas_amount(service_with_config.initial_gas_amount)
-                    service_with_meta = service_with_config.service
-                    second_partition_dir = next(parse_iterator)
-                    if type(second_partition_dir) is not str: raise Exception('Error: fail sending service.')
-                    try:
-                        cost = execution_cost(
-                            service_buffer=get_service_buffer_from_registry(
-                                service_hash=save_service(
-                                    service_with_meta_dir=service_with_config.service,  # TODO
-                                    service_hash=service_hash
-                                )
-                            ),
-                            metadata=service_with_meta.metadata
-                        ) * GAS_COST_FACTOR
-                    except build.UnsupportedArchitectureException as e:
-                        raise e
-                    break                
-                """
-
-        if not initial_service_cost:
-            initial_service_cost: int = default_initial_cost(
-                father_id=client_id if client_id else get_only_the_ip_from_context(context_peer=context.peer())
-            )
-        cost: int = cost + initial_service_cost if cost else initial_service_cost
-        l.LOGGER('Execution cost for a service is requested, cost -> ' + str(cost) + ' with benefit ' + str(0))
-        if cost is None:
-            raise Exception("I dont've the service.")
-        for b in grpcbf.serialize_to_buffer(
-                message_iterator=gateway_pb2.EstimatedCost(
-                    cost=to_gas_amount(cost),
-                    variance=0  # TODO dynamic variance.
-                ),
-                indices=gateway_pb2.EstimatedCost
-        ): yield b
+        yield from GetServiceEstimatedCostIterable(request_iterator, context)
 
     def Payable(self, request_iterator, context, **kwargs):
         l.LOGGER('Request for payment.')
