62120462193094b2ce0702a2dabb5dc0410d6393
jossemii <jossemii@proton.me>
2024-01-14 13:05:02 +0100

    control if reputation_graph rust lib is not installled (because manually is needed)

--- src/reputation_system/simple_reputation_feedback.py
+++ src/reputation_system/simple_reputation_feedback.py
@@ -1,15 +1,25 @@
-from reputation_graph import compute as lib_compute, spend as lib_spend
 
-from src.utils.env import DOCKER_NETWORK
-from src.utils.utils import get_network_name
+try:
+    from reputation_graph import compute as lib_compute, spend as lib_spend
 
-
-def submit_reputation_feedback(token: str, amount: int):
-    # Take the peer_id when the token it's external. Do nothing if it's an external service.
-    if get_network_name(ip_or_uri=token.split('##')[1]) == DOCKER_NETWORK:
-        pointer = token.split('##')[1]
-        lib_spend("", amount, pointer)
+    from src.utils.env import DOCKER_NETWORK
+    from src.utils.utils import get_network_name
 
 
-def compute_reputation_feedback(pointer):
-    return lib_compute(pointer, pointer)
+    def submit_reputation_feedback(token: str, amount: int):
+        # Take the peer_id when the token it's external. Do nothing if it's an external service.
+        if get_network_name(ip_or_uri=token.split('##')[1]) == DOCKER_NETWORK:
+            pointer = token.split('##')[1]
+            lib_spend("", amount, pointer)
+
+
+    def compute_reputation_feedback(pointer):
+        return lib_compute(pointer, pointer)
+
+except ModuleNotFoundError:
+    def submit_reputation_feedback(token: str, amount: int):
+        pass
+
+
+    def compute_reputation_feedback(pointer):
+        pass
