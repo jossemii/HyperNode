5031aa71f79a254f15f7e2b0a7a3b0f0330026d7
jossemii <josemi.bnf@gmail.com>
2024-02-04 13:28:33 +0100

    don't raise exception if the token is from an internal container

--- src/reputation_system/simple_reputation_feedback.py
+++ src/reputation_system/simple_reputation_feedback.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 try:
     from sigma_reputation_graph import compute as lib_compute, spend as lib_spend
 
@@ -5,12 +8,11 @@ try:
     from src.utils.utils import get_network_name
 
 
-    def submit_reputation_feedback(token: str, amount: int) -> str:
+    def submit_reputation_feedback(token: str, amount: int) -> Optional[str]:
         # Take the peer_id when the token it's external. Do nothing if it's an external service.
         pointer: str = token.split('##')[1]
-        if get_network_name(ip_or_uri=token.split('##')[1]) == DOCKER_NETWORK: 
-            raise Exception("Exception: Submit simple reputation feedback only for external tokens.")
-        return lib_spend("", amount, pointer)
+        if get_network_name(ip_or_uri=token.split('##')[1]) != DOCKER_NETWORK: 
+            return lib_spend("", amount, pointer)
 
     def compute_reputation_feedback(pointer) -> float:
         _result: float = lib_compute(None, pointer)
