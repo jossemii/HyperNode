def15284ffbba05bbd9c741e8eddc24f926e28bf
jossemii <jossemii@proton.me>
2024-03-08 18:22:21 +0100

    fixed command/compile

--- src/commands/compile/compile.py
+++ src/commands/compile/compile.py
@@ -9,6 +9,7 @@ from grpcbigbuffer import client as grpcbb
 from protos import celaut_pb2, compile_pb2, gateway_pb2_grpcbf, gateway_pb2_grpc
 from src.commands.compile.generate_service_zip import __generate_service_zip
 from src.database.access_functions.peers import get_peer_ids, get_peer_directions
+from src.utils.env import METADATA_REGISTRY, REGISTRY
 
 
 def __compile(zip, node: str):
@@ -38,11 +39,11 @@ def __on_peer(peer: str, service_zip_dir: str):
             _id = b.id.hex()
             print(f"El id es {_id}")
         elif type(b) == celaut_pb2.Any.Metadata and _id:
-            with open(f"__metadata__/{_id}", "wb") as f:
+            with open(f"{METADATA_REGISTRY}{_id}", "wb") as f:
                 f.write(b.SerializeToString())
         elif type(b) == grpcbb.Dir and b.type == compile_pb2.Service and _id:
             # b is ServiceWithMeta grpc-bb cache directory.
-            os.system('mv ' + b.dir + ' ' + '__registry__/' + _id)
+            os.system(f"mv {b.dir} {REGISTRY}{_id}")
         else:
             raise Exception('\nError with the compiler output:' + str(b))
 
@@ -54,7 +55,7 @@ def __on_peer(peer: str, service_zip_dir: str):
     validate_id = sha3_256()
 
     try:
-        for i in grpcbb.read_multiblock_directory('__registry__/' + _id + '/'):
+        for i in grpcbb.read_multiblock_directory(f"{REGISTRY}{_id}/"):
             validate_id.update(i)
     except Exception as e:
         print(f"Maybe it doesn't have blocks? {str(e)}")
