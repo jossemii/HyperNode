82b65ad898452f35af68897fb427b073726284c4
jse07 <jse07@anon.erg>
2024-03-29 23:43:54 +0100

    ram usage graph.

--- src/commands/ratatui/peer-list/src/app.rs
+++ src/commands/ratatui/peer-list/src/app.rs
@@ -1,10 +1,12 @@
 use std::error;
 use rusqlite::{Connection, Result};
+use sysinfo::System;
 
 /// Application result type.
 pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 const DATABASE_FILE: &str = "../../../../storage/database.sqlite";
+pub const RAM_TIMES: usize = 500;
 
 #[derive(Debug)]
 pub struct Peer {
@@ -34,19 +36,45 @@ fn get_peers() -> Result<Vec<Peer>> {
     )
 }
 
+fn get_ram_usage() -> u64 {
+    // Create a System object
+    let mut sys = System::new_all();
+
+    // First we update all information of our system struct.
+    sys.refresh_all();
+
+    // Get total and used memory
+    let total_memory = sys.total_memory();
+    let used_memory = sys.used_memory();
+
+    // Calculate the RAM usage in percentage
+    let ram_usage_percentage = (used_memory as f64 / total_memory as f64) * 100.0;
+    ram_usage_percentage as u64 
+}
+
+#[derive(Debug)]
+pub enum Tabs {
+    Peers,
+    Containers
+}
+
 /// Application.
 #[derive(Debug)]
 pub struct App {
     /// Is the application running?
     pub running: bool,
-    pub peers: Vec<Peer>
+    pub tab: Tabs,
+    pub peers: Vec<Peer>,
+    pub ram_usage: Vec<u64>
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
             running: true,
-            peers: get_peers().unwrap_or_default()
+            tab: Tabs::Peers,
+            peers: get_peers().unwrap_or_default(),
+            ram_usage: [0; RAM_TIMES].to_vec()
         }
     }
 }
@@ -66,6 +94,7 @@ impl App {
     }
 
     pub fn refresh(&mut self) {
-        self.peers = get_peers().unwrap_or_default()
+        self.peers = get_peers().unwrap_or_default();
+        self.ram_usage.push(get_ram_usage());
     }
 }
--- src/commands/ratatui/peer-list/src/ui.rs
+++ src/commands/ratatui/peer-list/src/ui.rs
@@ -1,9 +1,11 @@
 use ratatui::{
-    layout::{Alignment, Constraint},
-    style::{Color, Style},
-    widgets::{Block, BorderType, Cell, Row, Table},
+    layout::{Alignment, Constraint, Rect},
+    style::{Color, Style, Stylize},
+    widgets::{Block, BorderType, Borders, Cell, RenderDirection, Row, Sparkline, Table, TableState},
     Frame,
 };
+use vec_to_array::vec_to_array;
+use crate::app::RAM_TIMES;
 
 use crate::app::App;
 
@@ -14,7 +16,8 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui-org/ratatui/tree/master/
 
-    frame.render_widget(
+    let mut table_state = TableState::default();
+    frame.render_stateful_widget(
         Table::new(
             app.peers.iter().map(|peer| {
                 Row::new(vec![peer.id.clone(), peer.uri.clone(), peer.gas.to_string()])
@@ -39,6 +42,33 @@ pub fn render(app: &mut App, frame: &mut Frame) {
                 .border_type(BorderType::Thick),
         )
         .style(Style::default().fg(Color::Cyan).bg(Color::Black)),
-        frame.size(),
-    )
+        Rect::new(0, 0, frame.size().width, frame.size().height/2),
+        &mut table_state
+    );
+
+    let ram_usage_arr: [u64; RAM_TIMES]  = {
+        if app.ram_usage.len() > RAM_TIMES {
+            let ram_usage_vector = app.ram_usage.clone()[(app.ram_usage.len() - RAM_TIMES)..].to_vec();
+            let ram_usage_arr: [u64; RAM_TIMES]  = vec_to_array!(ram_usage_vector, u64, RAM_TIMES);
+            ram_usage_arr
+        }else {
+            [0; RAM_TIMES]
+        }
+    };
+    frame.render_widget(
+        Sparkline::default()
+            .block(Block::default().title("Sparkline").borders(Borders::ALL))
+            .data(&ram_usage_arr)
+            .max(100)
+            .direction(RenderDirection::LeftToRight)
+            .style(Style::default().light_yellow().on_white())
+        .block(
+            Block::bordered()
+                .title("Ram usage")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Thick),
+        )
+        .style(Style::default().fg(Color::Cyan).bg(Color::Black)),
+        Rect::new(0, frame.size().height/2, frame.size().width, frame.size().height/2)
+    );
 }
