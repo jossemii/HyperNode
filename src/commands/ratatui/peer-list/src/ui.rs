339ecea607ad33cc9ab6ffcdeec3d2e158a7963c
jse07 <jse07@anon.erg>
2024-03-29 19:14:44 +0100

    asd.

--- src/commands/ratatui/peer-list/src/ui.rs
+++ src/commands/ratatui/peer-list/src/ui.rs
@@ -1,34 +1,82 @@
+use std::fmt::format;
+
 use ratatui::{
-    layout::Alignment,
+    layout::{Alignment, Constraint},
     style::{Color, Style},
-    widgets::{Block, BorderType, Paragraph},
+    widgets::{Block, BorderType, Cell, Row, Table},
     Frame,
 };
 
 use crate::app::App;
+
+use rusqlite::{Connection, Result};
+
+const DATABASE_FILE: &str = "../../../../storage/database.sqlite";
+
+struct Peer {
+    id: String,
+    uri: String,
+    gas: u8
+}
+
+fn get_peer() -> Result<Vec<Peer>> {
+    Ok(
+        Connection::open(DATABASE_FILE)?.prepare(
+            "SELECT p.id, u.ip, u.port
+            FROM peer p
+            JOIN slot s ON p.id = s.peer_id
+            JOIN uri u ON s.id = u.slot_id",
+        )?.query_map([], |row| {
+            let id: String = row.get(0)?;
+            let ip: String = row.get(1)?;
+            let port: u16 = row.get(2)?;
+            Ok(Peer {
+                id: id,
+                uri: format!("{}:{}", ip, port),
+                gas: 0
+            })
+        })?
+        .collect::<Result<Vec<Peer>>>()?
+    )
+}
 
 /// Renders the user interface widgets.
 pub fn render(app: &mut App, frame: &mut Frame) {
     // This is where you add new widgets.
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
-    // - https://github.com/ratatui-org/ratatui/tree/master/examples
+    // - https://github.com/ratatui-org/ratatui/tree/master/
+
     frame.render_widget(
-        Paragraph::new(format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            app.counter
-        ))
+        Table::new(
+            match get_peer() {
+                Ok(peer_ids) => {
+                    peer_ids.iter().map(|peer| {
+                        Row::new(vec![peer.id.clone(), peer.uri.clone(), peer.gas.to_string()])
+                    }).collect()
+                },
+                Err(e) => Vec::new(),
+            },
+            [
+                Constraint::Length(30),
+                Constraint::Length(30),
+                Constraint::Length(30),
+            ]
+        )
+        .header(
+            Row::new(vec![
+                Cell::from("Id"),
+                Cell::from("Main URI"),
+                Cell::from("Gas on it")
+            ])
+        )
         .block(
             Block::bordered()
-                .title("Template")
+                .title("Available peer list")
                 .title_alignment(Alignment::Center)
-                .border_type(BorderType::Rounded),
+                .border_type(BorderType::Thick),
         )
-        .style(Style::default().fg(Color::Cyan).bg(Color::Black))
-        .centered(),
+        .style(Style::default().fg(Color::Cyan).bg(Color::Black)),
         frame.size(),
     )
 }
