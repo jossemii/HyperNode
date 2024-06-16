7adc8c11d38aa3a72f2fba82d0bb8fa028137675
jse07 <40121100+jse07@users.noreply.github.com>
2024-06-16 13:00:57 +0200

    Update setup_ubuntu_arm.sh

--- bash/setup_ubuntu_arm.sh
+++ bash/setup_ubuntu_arm.sh
@@ -21,7 +21,9 @@ apt-get -y install \
     lsb-release
 
 # Add Docker GPG key and repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+if [ ! -f /usr/share/keyrings/docker-archive-keyring.gpg ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+fi
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package lists again
