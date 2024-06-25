49590e796efe0c3b4d1418f921205a445ed59277
jse07 <40121100+jse07@users.noreply.github.com>
2024-06-25 17:38:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-# Check if the script is running with root privileges
-if [ "$(id -u)" -ne 0 ]; then
-  echo "Error: This script needs to be run with sudo."
-  echo "Please run the following command to run this script with sudo:"
-  echo "  sudo $0"
-  exit 1
+# Ask for the target directory with a default value
+echo "Enter the target directory [default: /nodo]: "
+read TARGET_DIR
+TARGET_DIR=${TARGET_DIR:-/nodo}
+
+# Check if the target directory already exists and ask for confirmation before deleting
+if [ -d "$TARGET_DIR" ]; then
+  echo "Target directory $TARGET_DIR already exists. Do you want to delete it? [y/N]: "
+  read confirm
+  case "$confirm" in
+    [Yy]*)
+      echo "Removing existing directory $TARGET_DIR..."
+      sudo rm -rf "$TARGET_DIR"
+      ;;
+    *)
+      echo "Installation aborted. Please specify a different target directory."
+      exit 1
+      ;;
+  esac
 fi
 
 # Function to install git if it's not already installed
@@ -16,13 +29,13 @@ install_git_if_needed() {
     # Detect the operating system and install git
     if [ -x "$(command -v apt)" ]; then
       # Debian/Ubuntu-based
-      apt update && apt install -y git
+      sudo apt update && sudo apt install -y git
     elif [ -x "$(command -v yum)" ]; then
       # Red Hat/CentOS-based
-      yum install -y git
+      sudo yum install -y git
     elif [ -x "$(command -v dnf)" ]; then
       # Fedora-based
-      dnf install -y git
+      sudo dnf install -y git
     elif [ -x "$(command -v brew)" ]; then
       # macOS
       brew install git
@@ -39,36 +52,15 @@ install_git_if_needed
 # Define the repository URL
 REPO_URL="https://github.com/celaut-project/nodo.git"
 
-# Ask for the target directory with a default value
-echo "Enter the target directory [default: /nodo]: "
-read TARGET_DIR
-TARGET_DIR=${TARGET_DIR:-/nodo}
-
-# Check if the target directory already exists and ask for confirmation before deleting
-if [ -d "$TARGET_DIR" ]; then
-  echo "Target directory $TARGET_DIR already exists. Do you want to delete it? [y/N]: "
-  read confirm
-  case "$confirm" in
-    [Yy]*)
-      echo "Removing existing directory $TARGET_DIR..."
-      rm -rf "$TARGET_DIR"
-      ;;
-    *)
-      echo "Installation aborted. Please specify a different target directory."
-      exit 1
-      ;;
-  esac
-fi
-
 # Clone the repository into the target directory
 echo "Cloning repository from $REPO_URL into $TARGET_DIR..."
-if ! git clone $REPO_URL $TARGET_DIR; then
+if ! git clone $REPO_URL "$TARGET_DIR"; then
   echo "Error: Failed to clone the repository."
   exit 1
 fi
 
 # Navigate to the cloned repository directory
-cd $TARGET_DIR || { echo "Error: Failed to change directory to $TARGET_DIR."; exit 1; }
+cd "$TARGET_DIR" || { echo "Error: Failed to change directory to $TARGET_DIR."; exit 1; }
 
 # Check the platform architecture and set the setup script accordingly
 if [ "$(uname -m)" = "armv7l" ]; then
@@ -78,11 +70,11 @@ else
 fi
 
 # Make sure the setup script is executable
-chmod +x $SETUP_SCRIPT
+chmod +x "$SETUP_SCRIPT"
 
 # Execute the setup script
 echo "Running setup script $SETUP_SCRIPT..."
-if ! ./$SETUP_SCRIPT; then
+if ! ./"$SETUP_SCRIPT"; then
   echo "Error: The setup script $SETUP_SCRIPT failed to execute."
   exit 1
 fi
@@ -98,9 +90,9 @@ create_service_file() {
     case "$confirm" in
       [Yy]*)
         echo "Stopping and removing existing service $SERVICE_FILE..."
-        systemctl stop nodo.service
-        systemctl disable nodo.service
-        rm -f "$SERVICE_FILE"
+        sudo systemctl stop nodo.service
+        sudo systemctl disable nodo.service
+        sudo rm -f "$SERVICE_FILE"
         ;;
       *)
         echo "Service setup aborted. Please manually handle the existing service file."
@@ -114,7 +106,7 @@ create_service_file() {
 
   # Create the service file
   echo "Creating $SERVICE_FILE..."
-  cat <<EOF > $SERVICE_FILE
+  sudo sh -c "cat <<EOF > $SERVICE_FILE
 [Unit]
 Description=Nodo Serve
 After=network.target
@@ -131,16 +123,16 @@ Environment=PYTHONUNBUFFERED=1
 
 [Install]
 WantedBy=multi-user.target
-EOF
+EOF"
 
   # Set the permissions for the service file
-  chmod 644 $SERVICE_FILE
+  sudo chmod 644 "$SERVICE_FILE"
 
   # Reload systemd, enable and start the service
   echo "Reloading systemd, enabling, and starting the nodo service..."
-  systemctl daemon-reload
-  systemctl enable nodo.service
-  systemctl start nodo.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable nodo.service
+  sudo systemctl start nodo.service
 }
 
 # Create nodo.service
