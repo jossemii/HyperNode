fba6d5e5b0e7906233156d654204766bd2fd075f
jse07 <40121100+jse07@users.noreply.github.com>
2024-06-26 08:17:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ After=network.target
 
 [Service]
 Type=simple
-User=$SCRIPT_USER
+User=root
 Group=sudo
 WorkingDirectory=$TARGET_DIR
 ExecStart=/bin/bash -c 'source $TARGET_DIR/venv/bin/activate && exec python3 $TARGET_DIR/nodo.py serve'
