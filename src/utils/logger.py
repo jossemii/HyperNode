feb3476d996de2457ee82410ae7970829f061d25
jse07 <40121100+jse07@users.noreply.github.com>
2024-06-30 17:06:21 +0200

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,7 +2,7 @@ import logging, os
 
 from src.utils.env import GET_ENV, STORAGE
 
-USE_PRINT = GET_ENV(env='USE_PRINT', default=True)
+USE_PRINT = GET_ENV(env='USE_PRINT', default=False)
 if not os.path.exists(STORAGE): os.makedirs(STORAGE)
 
 logging.basicConfig(filename=f'{STORAGE}/app.log', level=logging.INFO,
