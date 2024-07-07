9471e303424808374bc1ca77ea4a473fa1403e05
jse07 <jse@anon.com>
2024-07-07 20:24:15 +0200

    sql_connection improve

--- src/database/sql_connection.py
+++ src/database/sql_connection.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import time
 from threading import Lock
-from typing import List, Tuple, TypedDict, Optional
+from typing import List, Tuple, Optional
 
 import docker as docker_lib
 import grpc
@@ -10,13 +10,21 @@ from grpcbigbuffer import client as grpcbf
 
 from protos import gateway_pb2_grpc, gateway_pb2
 from src.utils import logger as l, logger
-from src.utils.env import CLIENT_MIN_GAS_AMOUNT_TO_RESET_EXPIRATION_TIME, CLIENT_EXPIRATION_TIME, DOCKER_CLIENT, \
-    DOCKER_NETWORK, REMOVE_CONTAINERS, STORAGE, DATABASE_FILE, DEFAULT_INTIAL_GAS_AMOUNT
+from src.utils.env import (
+    CLIENT_MIN_GAS_AMOUNT_TO_RESET_EXPIRATION_TIME,
+    CLIENT_EXPIRATION_TIME,
+    DOCKER_CLIENT,
+    REMOVE_CONTAINERS,
+    STORAGE,
+    DATABASE_FILE,
+    DEFAULT_INTIAL_GAS_AMOUNT
+)
 from src.utils.singleton import Singleton
-from src.utils.utils import get_network_name, from_gas_amount, generate_uris_by_peer_id
+from src.utils.utils import from_gas_amount, generate_uris_by_peer_id
 
 
 def get_internal_service_id_by_token(token: str) -> str:
+    """Extracts the internal service ID from a token."""
     return token.split("##")[2]
 
 class SQLConnection(metaclass=Singleton):
@@ -24,6 +32,7 @@ class SQLConnection(metaclass=Singleton):
     _lock = Lock()
 
     def __init__(self):
+        """Initializes the SQLConnection, ensuring storage directory and establishing a database connection."""
         if not os.path.exists(STORAGE):
             os.makedirs(STORAGE)
         if SQLConnection._connection is None:
@@ -31,8 +40,17 @@ class SQLConnection(metaclass=Singleton):
             SQLConnection._connection.row_factory = sqlite3.Row
             self.cursor = SQLConnection._connection.cursor()
 
-    # Método para asegurar la conexión persistente
-    def _execute(self, query, params=()):
+    def _execute(self, query: str, params=()) -> sqlite3.Cursor:
+        """
+        Executes a query with the given parameters, ensuring thread safety.
+
+        Args:
+            query (str): The SQL query to execute.
+            params (tuple): The parameters to bind to the query.
+
+        Returns:
+            sqlite3.Cursor: The cursor for the executed query.
+        """
         with SQLConnection._lock:
             try:
                 self.cursor.execute(query, params)
@@ -42,7 +60,7 @@ class SQLConnection(metaclass=Singleton):
                 raise e
             return self.cursor
 
-    # Clientes
+    # Client Methods
 
     def get_clients(self) -> List[dict]:
         """
@@ -71,15 +89,32 @@ class SQLConnection(metaclass=Singleton):
         return [row['id'] for row in result.fetchall()]
 
     def client_exists(self, client_id: str) -> bool:
-        # Check if the peer exists in the database
+        """
+        Checks if a client exists in the database.
+
+        Args:
+            client_id (str): The ID of the client to check.
+
+        Returns:
+            bool: True if the client exists, False otherwise.
+        """
         result = self._execute('''
             SELECT COUNT(*)
             FROM clients
             WHERE id = ?
         ''', (client_id,))
-        return result.fetchone()[0]
+        return result.fetchone()[0] > 0
 
     def get_client_gas(self, client_id: str) -> Tuple[int, float]:
+        """
+        Retrieves the gas and last usage time for a client.
+
+        Args:
+            client_id (str): The ID of the client.
+
+        Returns:
+            Tuple[int, float]: The gas amount and last usage time.
+        """
         result = self._execute('''
             SELECT gas, last_usage FROM clients WHERE id = ?
         ''', (client_id,))
@@ -89,16 +124,25 @@ class SQLConnection(metaclass=Singleton):
         raise Exception(f'Client not found: {client_id}')
 
     def __update_client(self, client_id: str, gas: int, last_usage: float):
+        """Updates the gas and last usage time for a client."""
         self._execute('''
             UPDATE clients SET gas = ?, last_usage = ? WHERE id = ?
         ''', (gas, last_usage, client_id))
 
     def delete_client(self, client_id: str):
+        """Deletes a client from the database."""
         self._execute('''
             DELETE FROM clients WHERE id = ?
         ''', (client_id,))
 
     def add_gas(self, client_id: str, gas: int = 0):
+        """
+        Adds gas to a client's balance.
+
+        Args:
+            client_id (str): The ID of the client.
+            gas (int): The amount of gas to add.
+        """
         _gas, _last_usage = self.get_client_gas(client_id)
         gas += _gas
         if _last_usage and gas >= CLIENT_MIN_GAS_AMOUNT_TO_RESET_EXPIRATION_TIME:
@@ -106,6 +150,13 @@ class SQLConnection(metaclass=Singleton):
         self.__update_client(client_id, gas, _last_usage)
 
     def reduce_gas(self, client_id: str, gas: int):
+        """
+        Reduces gas from a client's balance.
+
+        Args:
+            client_id (str): The ID of the client.
+            gas (int): The amount of gas to reduce.
+        """
         _gas, _last_usage = self.get_client_gas(client_id)
         _gas -= gas
         if _gas == 0 and _last_usage is None:
@@ -113,6 +164,16 @@ class SQLConnection(metaclass=Singleton):
         self.__update_client(client_id, _gas, _last_usage)
 
     def update_sys_req(self, token: str, mem_limit: Optional[int]) -> bool:
+        """
+        Updates system requirements for an internal service.
+
+        Args:
+            token (str): The token of the internal service.
+            mem_limit (Optional[int]): The new memory limit.
+
+        Returns:
+            bool: True if update was successful, False otherwise.
+        """
         try:
             self._execute('''
                 UPDATE internal_services SET mem_limit = ? WHERE id = ?
@@ -122,6 +183,15 @@ class SQLConnection(metaclass=Singleton):
             return False
 
     def get_sys_req(self, token: str) -> dict:
+        """
+        Retrieves system requirements for an internal service.
+
+        Args:
+            token (str): The token of the internal service.
+
+        Returns:
+            dict: A dictionary containing the system requirements.
+        """
         result = self._execute('''
             SELECT mem_limit FROM internal_services WHERE id = ?
         ''', (get_internal_service_id_by_token(token=token),))
@@ -131,6 +201,15 @@ class SQLConnection(metaclass=Singleton):
         raise Exception(f'Internal service {token}')
 
     def get_internal_service_gas(self, token: str) -> int:
+        """
+        Retrieves the gas amount for an internal service.
+
+        Args:
+            token (str): The token of the internal service.
+
+        Returns:
+            int: The gas amount.
+        """
         result = self._execute('''
             SELECT gas FROM internal_services WHERE id = ?
         ''', (get_internal_service_id_by_token(token=token),))
@@ -141,7 +220,7 @@ class SQLConnection(metaclass=Singleton):
 
     def get_all_internal_service_tokens(self) -> List[str]:
         """
-        Fetches all tokens of the internal services in the format `father_id##ip##id`.
+        Fetches all tokens of internal services.
 
         Returns:
             List[str]: A list of tokens.
@@ -152,12 +231,27 @@ class SQLConnection(metaclass=Singleton):
         return [f"{row['father_id']}##{row['ip']}##{row['id']}" for row in result.fetchall()]
 
     def client_expired(self, client_id: str) -> bool:
+        """
+        Checks if a client has expired.
+
+        Args:
+            client_id (str): The ID of the client.
+
+        Returns:
+            bool: True if the client has expired, False otherwise.
+        """
         _gas, _last_usage = self.get_client_gas(client_id)
         return _last_usage is not None and ((time.time() - _last_usage) >= CLIENT_EXPIRATION_TIME)
 
-    # Peers
+    # Peer Methods
 
     def get_peers(self) -> List[dict]:
+        """
+        Fetches all peers from the database.
+
+        Returns:
+            List[dict]: A list of dictionaries containing peer details.
+        """
         result = self._execute('''
             SELECT id, token, client_id, gas FROM peer
         ''')
@@ -170,12 +264,8 @@ class SQLConnection(metaclass=Singleton):
         Returns:
             List[str]: A list of peer IDs.
         """
-
-        # Query to select all peer IDs
-        query = "SELECT id FROM peer"
-
         try:
-            result = self._execute(query)
+            result = self._execute("SELECT id FROM peer")
             peer_ids = [row[0] for row in result.fetchall()]
             logger.LOGGER(f'Found peer IDs: {peer_ids}')
             return peer_ids
@@ -185,9 +275,7 @@ class SQLConnection(metaclass=Singleton):
 
     def add_peer(self, peer_id: str) -> bool:
         """
-        Adds a peer to the database. Checks if the peer already exists, and if not,
-        inserts a new record with the peer_id. The client_id is added separately
-        once it is generated.
+        Adds a peer to the database.
 
         Args:
             peer_id (str): The ID of the peer to add.
@@ -197,7 +285,6 @@ class SQLConnection(metaclass=Singleton):
         """
         logger.LOGGER(f'Attempting to add peer {peer_id}')
 
-        # If the peer does not exist, insert it
         if not self.peer_exists(peer_id=peer_id):
             try:
                 self._execute('''
@@ -214,39 +301,42 @@ class SQLConnection(metaclass=Singleton):
             return False
 
     def peer_exists(self, peer_id: str) -> bool:
-        # Check if the peer exists in the database
+        """
+        Checks if a peer exists in the database.
+
+        Args:
+            peer_id (str): The ID of the peer to check.
+
+        Returns:
+            bool: True if the peer exists, False otherwise.
+        """
         result = self._execute('''
             SELECT COUNT(*)
             FROM peer
             WHERE id = ?
         ''', (peer_id,))
-        return result.fetchone()[0]
+        return result.fetchone()[0] > 0
 
     def add_external_client(self, peer_id: str, client_id: str) -> bool:
         """
         Associates an external client ID with an existing peer.
 
         Args:
-            peer_id (str): The ID of the peer to associate with the client.
-            client_id (str): The external client ID to associate with the peer.
+            peer_id (str): The ID of the peer.
+            client_id (str): The external client ID to associate.
 
         Returns:
             bool: True if the association was successful, False otherwise.
         """
         logger.LOGGER(f'Attempting to add external client {client_id} for peer {peer_id}')
 
-        peer_exists = self.peer_exists(peer_id=peer_id)
-
-        if not peer_exists:
+        if not self.peer_exists(peer_id=peer_id):
             logger.LOGGER(f'Peer {peer_id} does not exist in the database')
             return False
 
-        # Update the peer to associate with the new client
         try:
             self._execute('''
-                UPDATE peer
-                SET client_id = ?
-                WHERE id = ?
+                UPDATE peer SET client_id = ? WHERE id = ?
             ''', (client_id, peer_id))
             logger.LOGGER(f'Associated external client {client_id} with peer {peer_id}')
             return True
@@ -254,32 +344,51 @@ class SQLConnection(metaclass=Singleton):
             logger.LOGGER(f'Failed to associate external client {client_id} with peer {peer_id}: {e}')
             return False
 
-    # Método para agregar datos a clients
     def add_client(self, client_id: str, gas: int, last_usage: Optional[float]):
+        """
+        Adds a client to the database, updating if a conflict occurs.
+
+        Args:
+            client_id (str): The ID of the client.
+            gas (int): The gas amount.
+            last_usage (Optional[float]): The last usage time.
+        """
         self._execute('''
             INSERT INTO clients (id, gas, last_usage)
             VALUES (?, ?, ?)
             ON CONFLICT(id) DO UPDATE SET gas=excluded.gas, last_usage=excluded.last_usage
         ''', (client_id, gas, last_usage))
 
-    # Método para obtener el token por uri
     def get_token_by_uri(self, uri: str) -> str:
+        """
+        Retrieves the token for a given URI.
+
+        Args:
+            uri (str): The URI to look up.
+
+        Returns:
+            str: The associated token.
+        """
         result = self._execute('''
-            SELECT token
-            FROM internal_services
-            WHERE ip = ?
+            SELECT token FROM internal_services WHERE ip = ?
         ''', (uri,))
         row = result.fetchone()
         if row:
             return row['token']
         raise Exception(f'Token not found for URI: {uri}')
 
-    # Método para obtener la cantidad de gas por id
     def get_gas_amount_by_client_id(self, id: str) -> int:
+        """
+        Retrieves the gas amount for a client ID.
+
+        Args:
+            id (str): The client ID.
+
+        Returns:
+            int: The gas amount.
+        """
         result = self._execute('''
-            SELECT gas
-            FROM clients
-            WHERE id = ?
+            SELECT gas FROM clients WHERE id = ?
         ''', (id,))
         row = result.fetchone()
         if row:
@@ -287,6 +396,15 @@ class SQLConnection(metaclass=Singleton):
         raise Exception(f'Gas amount not found for ID: {id}')
 
     def get_gas_amount_by_father_id(self, id: str) -> int:
+        """
+        Retrieves the gas amount for a father ID, checking both clients and internal services.
+
+        Args:
+            id (str): The father ID.
+
+        Returns:
+            int: The gas amount.
+        """
         if self.client_exists(client_id=id):
             return self.get_gas_amount_by_client_id(id=id)
         elif self.container_exists(token=id):
@@ -294,13 +412,17 @@ class SQLConnection(metaclass=Singleton):
         else:
             return int(DEFAULT_INTIAL_GAS_AMOUNT)
 
-    def add_internal_service(self,
-                             father_id: str,
-                             container_ip: str,
-                             container_id: str,
-                             token: str,
-                             gas: int
-                             ):
+    def add_internal_service(self, father_id: str, container_ip: str, container_id: str, token: str, gas: int):
+        """
+        Adds an internal service to the database.
+
+        Args:
+            father_id (str): The father ID.
+            container_ip (str): The IP address of the container.
+            container_id (str): The container ID.
+            token (str): The token.
+            gas (int): The gas amount.
+        """
         self._execute('''
             INSERT INTO internal_services (id, ip, father_id, gas, mem_limit)
             VALUES (?, ?, ?, ?, ?)
@@ -308,26 +430,62 @@ class SQLConnection(metaclass=Singleton):
         l.LOGGER(f'Set on cache {token} as dependency of {father_id}')
 
     def update_gas_to_container(self, token: str, gas: int):
+        """
+        Updates the gas amount for a container.
+
+        Args:
+            token (str): The token of the container.
+            gas (int): The new gas amount.
+        """
         self._execute('''
             UPDATE internal_services SET gas = ? WHERE id = ?
         ''', (gas, get_internal_service_id_by_token(token=token)))
 
     def container_exists(self, token: str) -> bool:
-        # Check if the peer exists in the database
+        """
+        Checks if a container exists in the database.
+
+        Args:
+            token (str): The token of the container.
+
+        Returns:
+            bool: True if the container exists, False otherwise.
+        """
         result = self._execute('''
             SELECT COUNT(*)
             FROM internal_services
             WHERE id = ?
         ''', (get_internal_service_id_by_token(token=token),))
-        return result.fetchone()[0]
+        return result.fetchone()[0] > 0
 
     def add_external_service(self, client_id: str, encrypted_external_token: str, external_token: str, peer_id: str):
+        """
+        Adds an external service to the database.
+
+        Args:
+            client_id (str): The client ID.
+            encrypted_external_token (str): The encrypted external token.
+            external_token (str): The external token.
+            peer_id (str): The peer ID.
+        """
         self._execute('''
             INSERT INTO external_services (token, token_hash, peer_id, client_id)
-            VALUES (?, ?, ?, ?, ?)
+            VALUES (?, ?, ?, ?)
         ''', (external_token, encrypted_external_token, peer_id, client_id))
 
     def purge_internal(self, agent_id=None, container_id=None, container_ip=None, token=None) -> int:
+        """
+        Purges an internal service and optionally removes its Docker container.
+
+        Args:
+            agent_id (str, optional): The agent ID.
+            container_id (str, optional): The container ID.
+            container_ip (str, optional): The container IP.
+            token (str, optional): The token of the internal service.
+
+        Returns:
+            int: The gas amount refunded.
+        """
         if token is None and (agent_id is None or container_id is None or container_ip is None):
             raise Exception(
                 'purge_internal: token is None and (agent_id is None or container_id is None or container_ip is None)'
@@ -343,24 +501,31 @@ class SQLConnection(metaclass=Singleton):
         refund = self.get_internal_service_gas(token=token)
 
         self._execute('''
-            DELETE FROM internal_services
-            WHERE id = ? AND father_id = ?
+            DELETE FROM internal_services WHERE id = ? AND father_id = ?
         ''', (container_id, agent_id))
 
         return refund
 
-    def purge_external(self, agent_id, peer_id, his_token) -> int:
+    def purge_external(self, agent_id: str, peer_id: str, his_token: str) -> int:
+        """
+        Purges an external service and refunds gas.
+
+        Args:
+            agent_id (str): The agent ID.
+            peer_id (str): The peer ID.
+            his_token (str): The token of the external service.
+
+        Returns:
+            int: The gas amount refunded.
+        """
         refund = 0
 
         hashed_token = self._execute('''
-            SELECT token_hash
-            FROM external_services
-            WHERE token = ?
+            SELECT token_hash FROM external_services WHERE token = ?
         ''', (his_token,)).fetchone()["token_hash"]
 
         self._execute('''
-            DELETE FROM external_services
-            WHERE token = ?
+            DELETE FROM external_services WHERE token = ?
         ''', (his_token,))
 
         try:
