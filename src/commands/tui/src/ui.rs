abb120622e61b2ca7a548b1cc9532e647c5f3edd
0xf965 <0xf965@non.exists>
2024-07-28 19:23:07 +0200

    changed i -> m

--- src/commands/tui/src/ui.rs
+++ src/commands/tui/src/ui.rs
@@ -119,7 +119,7 @@ fn get_controls_text(app: &App) -> String {
 
     control_text.push_str("Left/Right for menu  |  Up/Down for table rows");
     control_text.push_str("  |  Press 'o' and 'p' to rotate the block views sections");
-    control_text.push_str("  |  Press 'i' to change the block view layout");
+    control_text.push_str("  |  Press 'm' to change the block view layout");
 
     if is_row_selected {
         match app.tabs.index {
