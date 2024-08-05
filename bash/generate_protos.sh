57d646d5540044847e8bea19da78ecdd88c905fd
0xf965 <0xf965@non.exists>
2024-08-05 16:33:33 +0200

    updated protos

--- bash/generate_protos.sh
+++ bash/generate_protos.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 # celaut
-python3 -m grpc_tools.protoc -I. --python_out=. celaut.proto --experimental_allow_proto3_optional &&
+python3 -m grpc_tools.protoc -I./protos --python_out=./protos ./protos/celaut.proto --experimental_allow_proto3_optional &&
+
 # compile
-python3 -m grpc_tools.protoc -I. --python_out=. compile.proto --experimental_allow_proto3_optional &&
+python3 -m grpc_tools.protoc -I./protos --python_out=./protos ./protos/compile.proto --experimental_allow_proto3_optional &&
+
 # gateway
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. gateway.proto --experimental_allow_proto3_optional
+python3 -m grpc_tools.protoc -I./protos --python_out=./protos --grpc_python_out=./protos ./protos/gateway.proto --experimental_allow_proto3_optional
