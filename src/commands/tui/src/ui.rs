91f7d7cf2084a1053c0307d636f2f5c86c4c2800
0xf965 <0xf965@non.exists>
2024-08-16 21:53:20 +0200

    fixed ui.rs

--- src/commands/tui/src/ui.rs
+++ src/commands/tui/src/ui.rs
@@ -339,7 +339,7 @@ fn draw_tunnel_list(frame: &mut Frame, app: &mut App, area: Rect) {
                         tunnel.id.clone(),
                         tunnel.uri.clone(),
                         tunnel.service.clone(),
-                        tunnel.live ? "Live" : "Dead",
+                        if tunnel.live { "Live" } else { "Dead" },
                     ])
                 })
                 .collect::<Vec<Row>>(),
