d13802a2e0b1ee6863fff2bc7da7ff8afcacd511
0xf965 <0xf965@non.exists>
2024-08-16 16:08:24 +0200

    nodo deleted some commands.

--- src/commands/others/check_net.py
+++
@@ -1,8 +0,0 @@
-from time import sleep, localtime, strftime
-from subprocess import check_output
-while True:
-    t = localtime()
-    current_time = strftime("%H:%M:%S", t)
-    print(current_time)
-    print(current_time, check_output('netstat -natup | grep 8090'))
-    sleep(1)
--- src/commands/others/check_ram.py
+++
@@ -1,5 +0,0 @@
-import psutil
-from time import sleep
-while True:
-    print('RAM memory % used:', psutil.virtual_memory()[2], ' -- ', psutil.virtual_memory())
-    sleep(1)
