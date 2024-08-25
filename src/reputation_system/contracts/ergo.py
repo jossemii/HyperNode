3424b1920f34ab9f97c306a72c36c9b0e9308017
0xf965 <0xf965@non.exists>
2024-08-25 20:09:21 +0200

    ergo contract refactor

--- src/reputation_system/contracts/ergo.py
+++ src/reputation_system/contracts/ergo.py
@@ -7,14 +7,13 @@ from typing import List, TypedDict, Optional
 from jpype import *
 import java.lang
 
-from org.ergoplatform.appkit import *
-from org.ergoplatform.appkit.impl import *
+from org.ergoplatform.appkit import ErgoToken, ErgoValue, ConstantsBuilder
 
-
+# Constants
+DEFAULT_FEE = 1_000_000
 SAFE_MIN_BOX_VALUE = 1_000_000
-DEFAULT_TOKEN_AMOUNT = 1_000_000
+DEFAULT_TOKEN_AMOUNT = 1_000_000_000
 DEFAULT_TOKEN_LABEL = "reputation-proof-token"
-
 CONTRACT = """{
   proveDlog(SELF.R7[GroupElement].get) &&
   sigmaProp(SELF.tokens.size == 1) &&
@@ -29,26 +28,15 @@ CONTRACT = """{
   })
 }"""
 
+# Enum definitions
 class ProofObjectType(Enum):
     PlainText = "plain/txt-utf8"
     ProofByToken = "token-proof"
 
+# TypedDict definitions
 class ProofObject(TypedDict):
     type: ProofObjectType
     value: str
-
-# Initialize JVM before calling the decorated function
-def initialize_jvm(function):
-    def wrapper(*args, **kwargs):
-        try:
-            jpype.addClassPath('ergo.jar')
-            jpype.startJVM()
-        except OSError:
-            pass
-        finally:
-            res = function(*args, **kwargs)
-            return res
-    return wrapper
 
 class Token(TypedDict):
     token_id: str
@@ -61,12 +49,23 @@ class Registers(TypedDict):
     r7: Optional[str]
     r8: Optional[str]
 
-def input_box_to_dict(input_box: 'org.ergoplatform.appkit.InputBoxImpl') -> dict:
+# JVM initialization decorator
+def initialize_jvm(function):
+    def wrapper(*args, **kwargs):
+        if not jpype.isJVMStarted():
+            jpype.addClassPath('ergo.jar')
+            jpype.startJVM()
+        return function(*args, **kwargs)
+    return wrapper
+
+# Utility function to convert InputBox to dict
+def __input_box_to_dict(input_box: 'org.ergoplatform.appkit.InputBoxImpl') -> dict:
     return json.loads(str(input_box.toJson(True)))
 
-def build_proof_box(
+# Function to build the proof box
+def __build_proof_box(
     ergo: appkit.ErgoAppKit,
-    input_boxes: java.util.ArrayList,  # java.util.ArrayList[InputBox]
+    input_boxes: java.util.ArrayList,
     sender_address: str,
     token_amount: int = DEFAULT_TOKEN_AMOUNT,
     reputation_token_label: str = DEFAULT_TOKEN_LABEL,
@@ -79,116 +78,84 @@ def build_proof_box(
     return ergo._ctx.newTxBuilder() \
             .outBoxBuilder() \
                 .value(SAFE_MIN_BOX_VALUE) \
-                .tokens([
-                    ErgoToken(
-                        input_boxes.get(0).getId().toString(),
-                        jpype.JLong(token_amount)
-                    )
-                ]) \
+                .tokens([ErgoToken(input_boxes.get(0).getId().toString(), jpype.JLong(token_amount))]) \
                 .registers([
-                    ErgoValue.of(jpype.JString(reputation_token_label).getBytes("utf-8")),         # R4
-                    ErgoValue.of(jpype.JString(object_type_to_assign.value).getBytes("utf-8")),    # R5
-                    ErgoValue.of(jpype.JString(object_to_assign).getBytes("utf-8")),               # R6
-                    ErgoValue.of(jpype.JString(sender_address).getBytes("utf-8")),                 # R7
-                    ErgoValue.of(jpype.JBoolean(polarization))                                     # R8
+                    ErgoValue.of(jpype.JString(reputation_token_label).getBytes("utf-8")),
+                    ErgoValue.of(jpype.JString(object_type_to_assign.value).getBytes("utf-8")),
+                    ErgoValue.of(jpype.JString(object_to_assign).getBytes("utf-8")),
+                    ErgoValue.of(jpype.JString(sender_address).getBytes("utf-8")),
+                    ErgoValue.of(jpype.JBoolean(polarization))
                 ]) \
-                .contract(
-                    ergo._ctx.compileContract(
-                        ConstantsBuilder.empty(), CONTRACT
-                    )
-                ) \
+                .contract(ergo._ctx.compileContract(ConstantsBuilder.empty(), CONTRACT)) \
             .build()
 
 @initialize_jvm
-def create_reputation_proof_tx(ergo: appkit.ErgoAppKit, wallet_mnemonic: str, assigned_object: Optional[ProofObject], polarization: bool = True):
-    mnemonic_password: Optional[str] = None
-    prover_index: int = 0
-    return_signed: bool = True
-    fee: int = 1_000_000  # Fee in nanoErgs
+def __create_reputation_proof_tx(node_url: str, wallet_mnemonic: str, assigned_object: Optional[ProofObject], polarization: bool = True):
+    ergo = appkit.ErgoAppKit(node_url=node_url)
+    fee = DEFAULT_FEE  # Fee in nanoErgs
 
     # 1. Get the change address
-    mnemonic = ergo.getMnemonic(wallet_mnemonic=wallet_mnemonic, mnemonic_password=mnemonic_password)
+    mnemonic = ergo.getMnemonic(wallet_mnemonic=wallet_mnemonic, mnemonic_password=None)
     sender_address = ergo.getSenderAddress(index=0, wallet_mnemonic=mnemonic[1], wallet_password=mnemonic[2])
 
     # 2. Prepare transaction inputs (get UTXOs)
     input_boxes = ergo.getInputBoxCovering(amount_list=[fee], sender_address=sender_address)
-    print(f"Input boxes: {input_boxes}", flush=True)
+
+    # Select the input box with min value to avoid NotEnoughErgsError
+    selected_input_box = min(
+        (input_box for input_box in input_boxes if __input_box_to_dict(input_box)["value"] > 2 * SAFE_MIN_BOX_VALUE),
+        key=lambda ib: __input_box_to_dict(ib)["value"],
+        default=None
+    )
+
+    if not selected_input_box:
+        raise Exception("No input box available.")
+
+    selected_input_box_obj = __input_box_to_dict(selected_input_box)
+    selected_input_boxes = java.util.ArrayList([selected_input_box])
+
+    value_in_ergs = (selected_input_box_obj["value"] - fee - SAFE_MIN_BOX_VALUE) / 10**9
 
     # 3. Build transaction outputs
     outputs = []
 
-    #
-    # TODO: Make better code.
-    #
-    # Get the input box with min value to avoid NotEnoughErgsError.
-    _input_box = None
-    _input_box_obj = None
-    _input_boxes = None
-    for _ib in input_boxes:
-        _ib_obj = input_box_to_dict(_ib)
-        if _ib_obj["value"] > 2*SAFE_MIN_BOX_VALUE:
-            if not _input_box_obj or _input_box_obj and _input_box_obj["value"] >= _ib_obj["value"]:
-                _input_boxes = java.util.ArrayList([_ib])
-                _input_box, _input_box_obj = _ib, _ib_obj
-
-    if not _input_box or not _input_boxes or not _input_box_obj:
-        raise Exception("No input box available.")
-
-    print(f"Selected input box {_input_box_obj['boxId']}", flush=True)
-    value_in_ergs = (_input_box_obj["value"] - fee - SAFE_MIN_BOX_VALUE) / 10**9  # Convert from nanoErgs to Ergs
-    print(f"Requested value in Ergs: {value_in_ergs}", flush=True)
-
-    # Reputation proof output box.
-    proof_box = build_proof_box(
+    # Reputation proof output box
+    proof_box = __build_proof_box(
         ergo=ergo,
-        input_boxes=_input_boxes,
+        input_boxes=selected_input_boxes,
         sender_address=sender_address.toString(),
         assigned_object=assigned_object,
         polarization=polarization
     )
-    print(f"proof box -> {proof_box}", flush=True)
     if proof_box:
         outputs.append(proof_box)
 
-    # Basic wallet output box.
+    # Basic wallet output box
     output_boxes = ergo.buildOutBox(receiver_wallet_addresses=[sender_address.toString()], amount_list=[value_in_ergs])
-    if not output_boxes[0]:
-        print("Output box is null.", flush=True)
-        return None
-
-    outputs.extend(output_boxes)
-    print(f"Outputs: {outputs}", flush=True)
-    print(f"Output values: {[output.getValue() for output in outputs]}", flush=True)
+    if output_boxes[0]:
+        outputs.extend(output_boxes)
 
     # 4. Build and sign the transaction
     unsigned_tx = ergo.buildUnsignedTransaction(
-        input_box=_input_boxes,
+        input_box=selected_input_boxes,
         outBox=outputs,
         fee=fee / 10**9,
         sender_address=sender_address
     )
-    print(f"Unsigned transaction: {unsigned_tx}", flush=True)
-
-    signed_tx = ergo.signTransaction(unsigned_tx, mnemonic[0], prover_index)
-    print(f"Signed transaction: {signed_tx}", flush=True)
+    signed_tx = ergo.signTransaction(unsigned_tx, mnemonic[0], prover_index=0)
 
     # 5. Submit the transaction and return the ID
     tx_id = ergo.txId(signed_tx)
     print(f"Transaction ID: {tx_id}", flush=True)
 
-    return signed_tx if return_signed else tx_id
+    return signed_tx if True else tx_id
 
 if __name__ == "__main__":
     object_to_assign = "nodo-test-1"
-    polarization = True
-    assigned_object = ProofObject(
-        type=ProofObjectType.PlainText,
-        value=object_to_assign
-    )
+    assigned_object = ProofObject(type=ProofObjectType.PlainText, value=object_to_assign)
 
     wallet_mnemonic = "decline reward asthma enter three clean borrow repeat identify wisdom horn pull entire adapt neglect"
-    node_url: str = "http://213.239.193.208:9052/"  # MainNet or TestNet
-    ergo = appkit.ErgoAppKit(node_url=node_url)
+    node_url = "http://213.239.193.208:9052/"  # MainNet or TestNet
 
-    tx_id = create_reputation_proof_tx(ergo=ergo, wallet_mnemonic=wallet_mnemonic, assigned_object=assigned_object, polarization=polarization)
+    tx_id = __create_reputation_proof_tx(node_url=node_url, wallet_mnemonic=wallet_mnemonic, assigned_object=assigned_object, polarization=True)
     print(f"Transaction: {tx_id}", flush=True)
