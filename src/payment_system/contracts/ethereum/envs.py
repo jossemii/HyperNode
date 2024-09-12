665973d711988344c1bfcb551b74570989624fd5
0xf965 <0xf965@non.exists>
2024-09-12 13:29:58 +0200

    deleted evm deposit contract implementation

--- src/payment_system/contracts/ethereum/envs.py
+++
@@ -1,7 +0,0 @@
-ETH_PROVIDER = ""
-
-ETH_LEDGER = "mainnet"
-
-PARITY_FACTOR = 100
-# Factor de paridad entre el valor de gas (representación de los recursos del nodo)
-# y el activo de la red de procesamiento de pagos.
