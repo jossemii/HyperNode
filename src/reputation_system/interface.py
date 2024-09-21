acc01b5cc22c3f10b93c4aa6db0f2f09ddbf1776
0xf965 <0xf965@non.exists>
2024-09-22 00:16:16 +0200

    improved update reputation proof.

--- src/reputation_system/interface.py
+++ src/reputation_system/interface.py
@@ -6,12 +6,13 @@ from src.utils.utils import get_network_name
 from src.reputation_system.contracts.ergo import submit_reputation_proof
 
 
+sc = SQLConnection()
+
 def update_reputation(token: str, amount: int) -> Optional[str]:
     # Take the peer_id when the token it's external. Do nothing if it's an external service.
     peer_id: str = token.split('##')[1] if "##" in token else token
-    # Check if peer exists:
-    if get_network_name(ip_or_uri=token.split('##')[1]) != DOCKER_NETWORK:
-        return SQLConnection().update_reputation_peer(peer_id, amount)
+    if sc.peer_exists(peer_id=peer_id):
+        return sc.update_reputation_peer(peer_id, amount)
     
     # For services.
     # For clients.
@@ -22,11 +23,11 @@ def compute_reputation_feedback(peer_id) -> float:
     As an initial implementation, the node will only consider its own observations.
     Therefore, it will not take into account the reputation assigned by other peers for each of the pairs it interacts with.
     """
-    _result: float = SQLConnection().get_reputation(peer_id)
+    _result: float = sc.get_reputation(peer_id)
     LOGGER(f"Computed reputation: {_result}")
     return _result
 
 def submit_reputation():
-    SQLConnection().submit_to_ledger(
+    sc.submit_to_ledger(
         submit=lambda objects: submit_reputation_proof(objects=objects)
     )
