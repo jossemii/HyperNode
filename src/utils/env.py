82ed62b9a90aca4e8e76bb9bdf554534d53b1d86
0xf965 <0xf965@non.exists>
2024-10-02 19:20:23 +0200

    by default, donation percentage is 0

--- src/utils/env.py
+++ src/utils/env.py
@@ -168,7 +168,7 @@ env_manager.get_env("TOTAL_REPUTATION_TOKEN_AMOUNT", 1_000_000_000)
 env_manager.get_env("PAYMENT_MANAGER_ITERATION_TIME", 86_400)
 env_manager.get_env("REPUTATION_PROOF_ID", "")
 env_manager.get_env("ERGO_DONATION_WALLET", "9ejNy2qoifmzfCiDtEiyugthuXMriNNPhNKzzwjPtHnrK3esvbD")
-env_manager.get_env("ERGO_DONATION_PERCENTAGE", "0.01")
+env_manager.get_env("ERGO_DONATION_PERCENTAGE", "0.00")
 
 # Logging and Memory Settings
 env_manager.get_env("MEMORY_LOGS", False)
