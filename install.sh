7742cec48e4141fc0bd1b75389c2020e1b13f3dd
0xf965 <0xf965@non.exists>
2024-10-04 17:29:02 +0200

    abort interactive install.sh. Don't needed and makes problems

--- install.sh
+++ install.sh
@@ -204,12 +204,3 @@ fi
 
 printf "Installation and service setup completed successfully. The repository is located at $TARGET_DIR.\n"
 printf "********** You can now use the 'nodo' command. **********\n"
-
-# Run Nodo configuration script.
-# Accept KyA
-CONFIG_SCRIPT="bash/reconfig.sh"
-chmod +x "$CONFIG_SCRIPT"
-if ! ./"$CONFIG_SCRIPT"; then
-  printf "Error: The script $CONFIG_SCRIPT failed to execute.\n" >&2
-  exit 1
-fi
