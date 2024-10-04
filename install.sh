314e511e7c34adcb41b7f86df13a38aa41911075
0xf965 <0xf965@non.exists>
2024-10-04 16:53:22 +0200

    try to fix

--- install.sh
+++ install.sh
@@ -197,13 +197,19 @@ fi
 # Accept KyA
 ACCEPT_KYA_SCRIPT="bash/accept_kya.sh"
 chmod +x "$ACCEPT_KYA_SCRIPT"
-if ! ./"$ACCEPT_KYA_SCRIPT" "$TARGET_DIR"; then
+if ! bash "$ACCEPT_KYA_SCRIPT" "$TARGET_DIR"; then
   printf "Error: The script $ACCEPT_KYA_SCRIPT failed to execute.\n" >&2
   exit 1
 fi
 
-# Run Nodo configuration script.
-nodo config
-
 printf "Installation and service setup completed successfully. The repository is located at $TARGET_DIR.\n"
 printf "********** You can now use the 'nodo' command. **********\n"
+
+# Run Nodo configuration script.
+# Accept KyA
+CONFIG_SCRIPT="bash/reconfig.sh"
+chmod +x "$CONFIG_SCRIPT"
+if ! bash "$CONFIG_SCRIPT"; then
+  printf "Error: The script $CONFIG_SCRIPT failed to execute.\n" >&2
+  exit 1
+fi
