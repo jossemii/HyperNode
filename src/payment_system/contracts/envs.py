1b2821ed9a69588011f4b5597e9d13f995493858
0xf965 <40121100+0xf965@users.noreply.github.com>
2024-10-13 15:06:53 +0000

    disabled on code. ergo payments.

--- src/payment_system/contracts/envs.py
+++ src/payment_system/contracts/envs.py
@@ -19,20 +19,20 @@ contract_ledger = celaut_pb2.Service.Api.ContractLedger
 
 PAYMENT_PROCESS_VALIDATORS = {
     **({simulated.CONTRACT_HASH: simulated.payment_process_validator} if SIMULATED else {}),
-    ergo.CONTRACT_HASH: ergo.payment_process_validator
+    # ergo.CONTRACT_HASH: ergo.payment_process_validator
 }
 
 AVAILABLE_PAYMENT_PROCESS: Dict[contract_hash, Callable[[amount, token, ledger, contract_addr], contract_ledger]] = {
     **({simulated.CONTRACT_HASH: simulated.process_payment} if SIMULATED else {}),
-    ergo.CONTRACT_HASH: ergo.process_payment
+    # ergo.CONTRACT_HASH: ergo.process_payment
 }
 
 INIT_INTERFACES: Dict[contract_hash, Callable[[], None]] = {
-    ergo.CONTRACT_HASH: ergo.init
+    # ergo.CONTRACT_HASH: ergo.init
 }
 
 MANAGE_INTERFACES: Dict[contract_hash, Callable[[], None]] = {
-    ergo.CONTRACT_HASH: ergo.manager
+    # ergo.CONTRACT_HASH: ergo.manager
 }
 
 DEMOS = [simulated.CONTRACT_HASH] if SIMULATED else []
