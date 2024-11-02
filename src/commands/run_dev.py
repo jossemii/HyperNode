58ed3a275c11b6cd523295ca37570d5e9535117e
0xf965 <40121100+0xf965@users.noreply.github.com>
2024-11-02 12:46:01 +0000

    rundev needs run as root

--- src/commands/run_dev.py
+++ src/commands/run_dev.py
@@ -1,5 +1,11 @@
 from src.gateway.launcher.local_execution.dev_container import create_dev_container
+import os
 
 def run_dev(path: str):
+    # Check if script is run as root
+    if os.geteuid() != 0:
+        print("This script requires superuser privileges. Please run with sudo.")
+        return
+
     print(f"Executing container for development purposes, project: {path}")
     create_dev_container(service_path=path)
