1908645ea9716d5ecd6c954a200bce54fb9bc9eb
0xf965 <40121100+0xf965@users.noreply.github.com>
2024-11-02 10:30:05 +0000

    fixed grammar

--- src/commands/run_dev.py
+++ src/commands/run_dev.py
@@ -1,5 +1,5 @@
 from src.gateway.launcher.local_execution.dev_container import create_dev_container
 
 def run_dev(path: str):
-    print(f"Execute container for development purpouse, project: {path}")
+    print(f"Executing container for development purposes, project: {path}")
     create_dev_container(service_path=path)
