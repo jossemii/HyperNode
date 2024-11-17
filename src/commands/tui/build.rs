2c1ee771deb5a79c216a85ab620cf5e835b8d085
0xf965 <thisemaildoesnotexists@anon.erg>
2024-11-17 19:27:32 +0100

    corrected paths on build.rs

--- src/commands/tui/build.rs
+++ src/commands/tui/build.rs
@@ -1,10 +1,10 @@
 fn main() {
     // Tells cargo to rerun the build script if the .proto files change.
-    println!("cargo:rerun-if-changed=../../../../protos/celaut.proto");
+    println!("cargo:rerun-if-changed=../../../protos/celaut.proto");
 
     // Tell prost-build where to find the .proto file and output the Rust file.
     prost_build::Config::new()
-        .out_dir("../../../../protos")
-        .compile_protos(&["../../../../protos/celaut.proto"], &["src"])
+        .out_dir("../../../protos")
+        .compile_protos(&["../../../protos/celaut.proto"], &["src"])
         .expect("Failed to compile protobuf files");
 }
