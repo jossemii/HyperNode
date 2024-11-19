76b3a1ba2e0417b652582996639e119c3f68fd20
0xf965 <thisemaildoesnotexists@anon.erg>
2024-11-19 18:34:29 +0100

    Cargo.lock.  not in git.

--- nodo.py
+++ nodo.py
@@ -58,7 +58,6 @@ def check_rust_installation():
         # Try to run 'rustc --version' to check if Rust is installed
         subprocess.run(['rustc', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Rust is already installed.", flush=True)
-        subprocess.run(["rm", "-y", f"{MAIN_DIR}/src/commands/tui/Cargo.lock"])
     except (subprocess.CalledProcessError, FileNotFoundError):
         print("Installing Rust (Cargo)...", flush=True)
         try:
