710e8bd04dbebd5f5c2ba4bd1a5fee45cd0168a0
0xf965 <thisemaildoesnotexists@anon.erg>
2024-11-20 08:25:47 +0100

    export update.

--- src/commands/export.py
+++ src/commands/export.py
@@ -16,7 +16,7 @@ def export(service: str, path: str):
         path (str): The directory path where the output file should be saved.
     """
     # Create the full path for the output file
-    output_file = os.path.join(path, f"{service}.cell")
+    output_file = os.path.join(path, f"{service}.bee")  # bee-rpc file extension
 
     # Ensure the output directory exists
     os.makedirs(path, exist_ok=True)
@@ -26,11 +26,10 @@ def export(service: str, path: str):
         
         # Read chunks of data from the registry and write them to the file
         for buff in read_from_registry(filename=os.path.join(METADATA, service)):
-            print(f"Meta chunk {len(buff.chunk)}")
-            f.write(buff.chunk)
+            f.write(buff.SerializeToString())
         
         # Read chunks of data from the registry and write them to the file
-        # for buff in read_from_registry(filename=os.path.join(REGISTRY, service)):
-        #     f.write(buff.chunk)
+        for buff in read_from_registry(filename=os.path.join(REGISTRY, service)):
+            f.write(buff.SerializeToString())
 
     print(f"Export completed.")
