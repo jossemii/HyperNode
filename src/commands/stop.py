b64a77576984ac74da90d164d579c0f291809dc8
0xf965 <thisemaildoesnotexists@anon.erg>
2024-11-23 17:49:07 +0100

    check sudo on stop command.

--- src/commands/stop.py
+++ src/commands/stop.py
@@ -1,5 +1,4 @@
-
-
+import os
 from src.manager.manager import prune_container
 from src.utils.env import SHA3_256_ID, EnvManager
 
@@ -10,6 +9,11 @@ SHA3_256 = SHA3_256_ID.hex()
 
 
 def stop(instance: str):
+    # Check if script is run as root
+    if os.geteuid() != 0:
+        print("This script requires superuser privileges. Please run with sudo.")
+        return
+    
     if prune_container(token=instance):
         print(f"Service instance {instance} deleted.")
     else:
