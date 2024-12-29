462f45144b3c526557b58e89a708c9e96a0baf94
0xf965 <thisemaildoesnotexists@anon.erg>
2024-12-29 22:29:38 +0100

    fix: remove unnecessary logging statements in reputation proof transaction calculation

--- src/reputation_system/contracts/ergo.py
+++ src/reputation_system/contracts/ergo.py
@@ -110,15 +110,11 @@ def __create_reputation_proof_tx(node_url: str, wallet_mnemonic: str, proof_id: 
         _is_self = any(obj[0] for obj in objects)
         _num = len(objects) if not _is_self else len(objects) - 1
         _total = _expected_total_reputation if not _is_self else _expected_total_reputation - 1
-        
-        LOGGER(f"SELF: {_is_self}, NUM: {_num}, TOTAL: {_total}")
 
         objects = [
             (obj[0], _total / _num, obj[2]) if obj[0] else (obj[0], obj[1], obj[2])
             for obj in objects
         ]
-        
-    LOGGER(f"Objects: {objects}")
     
     total_token_value = int(sum([obj[1] for obj in objects]))
                 
