ec5a2cfb36e102bf654c3ce900dcbe67d38481a7
0xf965 <thisemaildoesnotexists@anon.erg>
2025-01-05 20:02:55 +0100

    fix: remove logging of available Ergo nodes in refresh command

--- nodo.py
+++ nodo.py
@@ -227,8 +227,7 @@ if __name__ == '__main__':
                 
             case 'refresh_ergo_nodes':
                 from src.manager.ergo import get_refresh_peers
-                _info = get_refresh_peers()
-                log.LOGGER(f"Available Ergo nodes: {_info}")
+                get_refresh_peers()
 
             case 'daemon':
                 from src.serve import serve
