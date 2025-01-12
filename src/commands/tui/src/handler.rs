12d98312e7a1aeb49df2759b9ebb7677230bc024
0xf965 <thisemaildoesnotexists@anon.erg>
2025-01-12 20:36:02 +0100

    using modifiers, code.

--- src/commands/tui/src/handler.rs
+++ src/commands/tui/src/handler.rs
@@ -19,36 +19,34 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App<'_>) -> AppRes
             _ => {}
         }
     } else {
-        match key_event.code {
-            KeyCode::Esc | KeyCode::Char('q') => {
+        match (key_event.modifiers, key_event.code) {
+            (KeyModifiers::NONE, KeyCode::Esc) | (KeyModifiers::NONE, KeyCode::Char('q')) => {
                 app.quit();
             }
-
-            KeyCode::Left => app.on_left(),
-            KeyCode::Up => app.on_up(),
-            KeyCode::Right => app.on_right(),
-            KeyCode::Down => app.on_down(),
-
-            KeyModifiers::SHIFT && KeyCode::Up => app.change_mode_view(),
-            KeyModifiers::SHIFT && KeyCode::Down => app.change_mode_view(),
-            KeyModifiers::SHIFT && KeyCode::Left => app.previous_block_view(),
-            KeyModifiers::SHIFT && KeyCode::Right => app.next_block_view(),
-
-            KeyCode::Char('d') => app.press_d().await,
-            KeyCode::Char('e') => app.press_e().await,
-
-            KeyCode::Char('c') | KeyCode::Char('C') => {
-                if key_event.modifiers == KeyModifiers::CONTROL {
-                    app.quit();
-                } else {
-                    if app.tabs.index == 0 {
-                        app.open_popup();
-                    }
+            (KeyModifiers::NONE, KeyCode::Left) => app.on_left(),
+            (KeyModifiers::NONE, KeyCode::Up) => app.on_up(),
+            (KeyModifiers::NONE, KeyCode::Right) => app.on_right(),
+            (KeyModifiers::NONE, KeyCode::Down) => app.on_down(),
+            
+            (KeyModifiers::SHIFT, KeyCode::Up) => app.change_mode_view(),
+            (KeyModifiers::SHIFT, KeyCode::Left) => app.previous_block_view(),
+            (KeyModifiers::SHIFT, KeyCode::Right) => app.next_block_view(),
+            
+            (KeyModifiers::NONE, KeyCode::Char('d')) => app.press_d().await,
+            (KeyModifiers::NONE, KeyCode::Char('e')) => app.press_e().await,
+        
+            (KeyModifiers::CONTROL, KeyCode::Char('c')) | (KeyModifiers::CONTROL, KeyCode::Char('C')) => {
+                app.quit();
+            }
+        
+            (KeyModifiers::NONE, KeyCode::Char('c')) | (KeyModifiers::NONE, KeyCode::Char('C')) => {
+                if app.tabs.index == 0 {
+                    app.open_popup();
                 }
             }
-
+        
             _ => {}
-        }   
+        } 
     }
     Ok(())
 }
