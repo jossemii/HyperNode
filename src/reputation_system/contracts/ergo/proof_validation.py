bd3288dff2a9397686eb211d7f897d87d9c419dc
0xf965 <thisemaildoesnotexists@anon.erg>
2025-01-13 08:59:41 +0100

    comment.

--- src/reputation_system/contracts/ergo/proof_validation.py
+++ src/reputation_system/contracts/ergo/proof_validation.py
@@ -157,5 +157,12 @@ def validate_reputation_proof_ownership() -> bool:
             f"Validation failed: The derived public key ({addr_pk}) does not match "
             f"the proof owner's public key ({proof_owner_pk})."
         ))
+        
+    """
+    Validation failed: The derived public key 
+    (ProveDlog(Ecp((8696f0bfa01ecf1244ae08579cbe486cf755d892de754cb674179bb3293b79c0,515cdaf7ae08876901334938490b625a4cf1792cd188d5c36f94ecd5c63a6c9d,1)))) 
+    does not match the proof owner's public key (038696f0bfa01ecf1244ae08579cbe486cf755d892de754cb674179bb3293b79c0).
+
+    """
     
     return valid
